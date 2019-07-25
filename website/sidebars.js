e695e8c1c1cc49cee92eb13477f25357b4b387f2
Omry Yadan <omry@fb.com>
2019-07-25 08:20:09 -0700

    initial website with docusaurus 2

---
+++ website/sidebars.js
@@ -0,0 +1,18 @@
+/**
+ * Copyright (c) 2017-present, Facebook, Inc.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
+module.exports = {
+    docs: {
+        'Getting started': [
+            'example-docs/doc10'
+        ],
+        'Getting started with docusaurus': [
+            'doc1', 'doc2', 'doc3'
+        ],
+        Features: ['mdx'],
+    },
+};
