fafd31538368eed2815bbd6d0cecb577ebad09d5
Omry Yadan <omry@fb.com>
2019-08-31 14:15:58 -0700

    fixed faircluster_defaults tests to do more than nothing

--- plugins/fair_cluster/tests/test_faircluster_defaults.py
+++ plugins/fair_cluster/tests/test_faircluster_defaults.py
@@ -1,4 +1,14 @@
-# # Copyright (c) Facebook, Inc. and its affiliates. All Rights Reserved
-# def test_fair_cluster_defaults():
-#     # TODO
-#     pass
+# Copyright (c) Facebook, Inc. and its affiliates. All Rights Reserved
+from hydra._internal.plugins import Plugins
+from hydra.plugins import SearchPathPlugin
+from hydra_plugins.fair_cluster.fair_cluster_defaults import FAIRClusterDefaults
+
+
+def test_discovery():
+    launchers = Plugins.discover(SearchPathPlugin)
+    # discovered plugins are actually different class objects, compare by name
+    assert FAIRClusterDefaults.__name__ in [x.__name__ for x in launchers]
+
+
+def test_fair_cluster_defaults():
+    pass
