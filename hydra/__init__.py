a62928c5c346d534417ccd09d2a511617536ada3
omry <omry@yadan.net>
2019-10-19 14:29:30 -0700

    1.0.0rc1

--- hydra/__init__.py
+++ hydra/__init__.py
@@ -4,6 +4,6 @@ from .errors import MissingConfigException
 from .main import main
 
 # Source of truth for Hydra's version
-__version__ = "0.9.0"
+__version__ = "1.0.0rc1"
 
 __all__ = ["__version__", "MissingConfigException", "main", "utils"]
