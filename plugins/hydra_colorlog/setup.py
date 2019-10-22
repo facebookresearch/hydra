8a7a1d2e13a5a136d4da442f773a29962bfb0829
Omry Yadan <omry@fb.com>
2019-10-21 21:01:51 -0700

    bump

--- plugins/hydra_colorlog/setup.py
+++ plugins/hydra_colorlog/setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
     LONG_DESC = fh.read()
     setup(
         name="hydra-colorlog",
-        version="0.1.0",
+        version="0.1.1",
         author="Omry Yadan",
         author_email="omry@fb.com",
         description="Enables colorlog for Hydra apps",
