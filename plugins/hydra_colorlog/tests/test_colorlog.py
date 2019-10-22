bc00bdb5fed818cf72978eb62a6b718a2d12a05c
Omry Yadan <omry@fb.com>
2019-10-21 20:44:39 -0700

    added test

--- plugins/hydra_colorlog/tests/test_colorlog.py
+++ plugins/hydra_colorlog/tests/test_colorlog.py
@@ -1,23 +1,18 @@
 # Copyright (c) Facebook, Inc. and its affiliates. All Rights Reserved
 
-# TODO
+from hydra._internal.hydra import Hydra
 
-# from hydra._internal.plugins import Plugins
-# from hydra_plugins.example_plugin import ExamplePlugin
-#
-#
-# def test_discovery():
-#     # This may seem weird, but it verifies that we can discover this Plugin via
-#     # the plugins subsystem.
-#     # The discover method takes a class and return all plugins that implements that class.
-#     # Typically we would be discovering all plugins that implementing some common interface.
-#     plugins = Plugins.discover(ExamplePlugin)
-#     # discovered plugins are actually different class objects, compare by name
-#     assert ExamplePlugin.__name__ in [x.__name__ for x in plugins]
-#
-#
-# def test_example_plugin():
-#     from hydra_plugins.example_plugin import ExamplePlugin
-#
-#     a = ExamplePlugin(10)
-#     assert a.add(20) == 30
+
+def test_config_installed():
+    """
+    Tests that color options are available for both hydra/hydra_logging and hydra/job_logging
+    """
+    hydra = Hydra(
+        calling_file=None,
+        calling_module="hydra_plugins.hydra_colorlog",
+        config_path="conf/",
+        task_function=None,
+        strict=False,
+    )
+    assert "color" in hydra.config_loader.get_group_options("hydra/job_logging")
+    assert "color" in hydra.config_loader.get_group_options("hydra/hydra_logging")
