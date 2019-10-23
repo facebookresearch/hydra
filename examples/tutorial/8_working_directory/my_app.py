dab87ee4cb549beb80146315d109308df0ed131a
Omry Yadan <omry@fb.com>
2019-10-23 13:50:13 -0700

    updated tests

--- examples/tutorial/8_working_directory/my_app.py
+++ examples/tutorial/8_working_directory/my_app.py
@@ -2,15 +2,11 @@
 import os
 
 import hydra
-from hydra import utils
 
 
 @hydra.main()
 def my_app(_cfg):
-    print("Current working directory  : {}".format(os.getcwd()))
-    print("Original working directory : {}".format(utils.get_original_cwd()))
-    print("to_absolute_path('foo')    : {}".format(utils.to_absolute_path("foo")))
-    print("to_absolute_path('/foo')   : {}".format(utils.to_absolute_path("/foo")))
+    print("Working directory : {}".format(os.getcwd()))
 
 
 if __name__ == "__main__":
