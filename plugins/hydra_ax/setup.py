1a34562b03e07b5effce4c652fac80b280844fc2
Shagun Sodhani <sodhani@fb.com>
2019-12-01 09:43:19 -0800

    Add Ax-platform and SQLAlchemy as requirements in the setup file

--- plugins/hydra_ax/setup.py
+++ plugins/hydra_ax/setup.py
@@ -1,5 +1,5 @@
 # Copyright (c) Facebook, Inc. and its affiliates. All Rights Reserved
-from setuptools import setup, find_packages
+from setuptools import find_packages, setup
 
 with open("README.md", "r") as fh:
     LONG_DESC = fh.read()
@@ -22,7 +22,7 @@ with open("README.md", "r") as fh:
             "Programming Language :: Python :: 3.7",
             "Operating System :: OS Independent",
         ],
-        install_requires=["hydra-core"],
+        install_requires=["hydra-core", "ax-platform==0.1.6", "SQLAlchemy==1.3.11"],
         # If this plugin is providing configuration files, be sure to include them in the package.
         # See MANIFEST.in.
         # For configurations to be discoverable at runtime, they should also be added to the search path.
