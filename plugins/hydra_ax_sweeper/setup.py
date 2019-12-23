b3cd36ab1875aefade54a2b3c30d3f5c209d0733
Shagun Sodhani <sodhani@fb.com>
2019-12-23 11:47:20 -0800

    Rename a module
    The name of the module was different from the name of the plugin. This
    caused some tests to fail.

--- plugins/hydra_ax_sweeper/setup.py
+++ plugins/hydra_ax_sweeper/setup.py
@@ -4,27 +4,26 @@ from setuptools import find_packages, setup
 with open("README.md", "r") as fh:
     LONG_DESC = fh.read()
     setup(
-        name="hydra-ax",
+        name="hydra-ax-sweeper",
         version="0.1.0",
-        author="Omry Yadan",
-        author_email="omry@fb.com",
+        author="Omry Yadan, Shagun Sodhani",
+        author_email="omry@fb.com, sshagunsodhani@gmail.com",
         description="Hydra Ax Sweeper plugin",
         long_description=LONG_DESC,
         long_description_content_type="text/markdown",
         url="https://github.com/facebookresearch/hydra/",
         packages=find_packages(exclude=["tests", "example"]),
         classifiers=[
-            # Feel free to choose another license
             "License :: OSI Approved :: MIT License",
-            # Python versions are used by the noxfile in Hydra to determine which
-            # python versions to test this plugin with
             "Programming Language :: Python :: 3.6",
             "Programming Language :: Python :: 3.7",
             "Operating System :: OS Independent",
         ],
-        install_requires=["hydra-core", "ax-platform==0.1.6", "SQLAlchemy==1.3.11"],
-        # If this plugin is providing configuration files, be sure to include them in the package.
-        # See MANIFEST.in.
-        # For configurations to be discoverable at runtime, they should also be added to the search path.
+        install_requires=[
+            "hydra-core",
+            "botorch==0.1.3",
+            "sqlalchemy",
+            "ax-platform==0.1.6",
+        ],
         include_package_data=True,
     )
