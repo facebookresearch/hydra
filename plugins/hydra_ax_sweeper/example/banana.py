bd22f93ac2433a0b65390cac5ea44222ad893f32
Shagun Sodhani <sodhani@fb.com>
2019-12-23 11:47:20 -0800

    Rename a module
    The name of the module was different from the name of the plugin. This
    caused some tests to fail.

--- plugins/hydra_ax_sweeper/example/banana.py
+++ plugins/hydra_ax_sweeper/example/banana.py
@@ -8,13 +8,10 @@ log = logging.getLogger(__name__)
 
 @hydra.main(config_path="conf/config.yaml")
 def banana(cfg):
-    print(cfg)
-    a = cfg.banana.a
-    b = cfg.banana.b
     x = cfg.banana.x
     y = cfg.banana.y
-    z = (a - x) ** 2 + b * ((y - x ** 2) ** 2)
-    log.info(f"Banana_Function_a={a}_b={b}(x={x}, y={y})={z}")
+    z = (1 - x) ** 2 + 1 * ((y - x ** 2) ** 2)
+    log.info(f"Banana_Function(x={x}, y={y})={z}")
     return z
 
 
