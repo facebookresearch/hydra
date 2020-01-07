08bcf595f79ae6d4063920f7cab67535f52f138d
Omry Yadan <omry@fb.com>
2020-01-07 14:25:08 -0800

    removed trailing slash from Docs footer link

--- website/docusaurus.config.js
+++ website/docusaurus.config.js
@@ -35,7 +35,7 @@ module.exports = {
                     items: [
                         {
                             label: 'Docs',
-                            to: 'docs/intro/'
+                            to: 'docs/intro'
                         },
                         {
                             label: 'Hydra@GitHub',
