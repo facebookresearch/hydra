47089d2077c3e13f7722e68073ab56e49c0af166
Shagun Sodhani <sodhani@fb.com>
2020-01-18 13:48:05 -0800

    Fix build errors
    * Use latest version of Ax
    * Fix incorrect package names
    * Add type hints
    * Remove all iterators

--- plugins/hydra_ax_sweeper/hydra_plugins/hydra_ax_sweeper/ax_sweeper.py
+++ plugins/hydra_ax_sweeper/hydra_plugins/hydra_ax_sweeper/ax_sweeper.py
@@ -1,37 +1,43 @@
 # Copyright (c) Facebook, Inc. and its affiliates. All Rights Reserved
 import logging
-from typing import List, Tuple
+from typing import Dict, List, Mapping, Optional, Tuple, Union
 
-from ax import ParameterType
-from ax.service.ax_client import AxClient
-from omegaconf import OmegaConf
+from ax import ParameterType  # type: ignore
+from ax.core import types as ax_types  # type: ignore
+from ax.service.ax_client import AxClient  # type: ignore
+from omegaconf import DictConfig, OmegaConf
 
-from hydra._internal.config_search_path import ConfigSearchPath
-from hydra._internal.plugins import Plugins
-from hydra.plugins import SearchPathPlugin, Sweeper
+from hydra._internal.config_search_path import ConfigSearchPath  # type: ignore
+from hydra._internal.plugins import Plugins  # type: ignore
+from hydra.core.config_loader import ConfigLoader
+from hydra.plugins import SearchPathPlugin, Sweeper  # type: ignore
+from hydra.types import TaskFunction
 
 log = logging.getLogger(__name__)
 
-Trial = List[str]
-BatchOfTrial = List[Trial]
+TrialType = Dict[str, int]
+BatchOfTrialType = List[TrialType]
+BestParameterType = Union[ax_types.Parameterization]
 
 # TODO: output directory is overwriting, job.num should be adjusted (depends on issue #284)
 # TODO: Support running multiple random seeds, aggregate mean and SEM
 
 
-class AxSweeperSearchPathPlugin(SearchPathPlugin):
+class AxSweeperSearchPathPlugin(SearchPathPlugin):  # type: ignore
     """
     This plugin makes the config files (provided by the AxSweeper plugin) discoverable and
     useable by the AxSweeper plugin.
     """
 
-    def manipulate_search_path(self, search_path):
+    def manipulate_search_path(self, search_path: str) -> None:
         assert isinstance(search_path, ConfigSearchPath)
         # Appends the search path for this plugin to the end of the search path
-        search_path.append("hydra-ax-sweeper", "pkg://hydra_plugins.ax_sweeper.conf")
+        search_path.append(
+            "hydra-ax-sweeper", "pkg://hydra_plugins.hydra_ax_sweeper.conf"
+        )
 
 
-def map_params_to_arg_list(params: dict) -> List[str]:
+def map_params_to_arg_list(params: Mapping[str, Union[str, float, int]]) -> List[str]:
     """Method to map a dictionary of params to a list of string arguments"""
     arg_list = []
     for key in params:
@@ -41,9 +47,20 @@ def map_params_to_arg_list(params: dict) -> List[str]:
 
 def get_batches_of_trials_given_parallelism(
     ax: AxClient, parallelism: Tuple[int, int], num_trials_left: int
-) -> List[BatchOfTrial]:
-    """Produce a batch of trials that can be run in parallel, given the parallelism"""
+) -> List[BatchOfTrialType]:
+
+    """Return a list of batch of trials.
+    Each batch of trials is a list where each trial in a batch can be run in paralle.
+    Different batches (of trials) should be executed sequentially, in the
+    order they are provided.
+
+    The difference between this method and `get_batches_of_trials_from_ax`
+    is that the former method returns a list of batches only for one
+    given `parallelism` while the latter method returns the list of batches
+    for all values of `parallelism`.
+    """
     num_trials, num_parallel_trials = parallelism
+    batches_of_trials = []
     if num_trials == -1:
         # This is a special case. We can use this parallelism as many times as we want.
 
@@ -51,17 +68,23 @@ def get_batches_of_trials_given_parallelism(
             # This is another special case where we can run as many trials in parallel as we want.
             # Given that num_trials is also -1, we can run all the trials in parallel.
             num_parallel_trials = num_trials_left
-        while True:
-            yield get_one_batch_of_trials(ax, num_parallel_trials)
+
+        while num_trials_left > 0:
+            batches_of_trials.append(
+                get_one_batch_of_trials(ax, num_parallel_trials)[:num_trials_left]
+            )
+            num_trials -= len(batches_of_trials[-1])
     else:
         if num_parallel_trials == -1:
             num_parallel_trials = num_trials
         number_of_parallel_trials = int(num_trials / num_parallel_trials)
         for batch_idx in range(number_of_parallel_trials):
-            yield get_one_batch_of_trials(ax, num_parallel_trials)
+            batches_of_trials.append(get_one_batch_of_trials(ax, num_parallel_trials))
+
+    return batches_of_trials
 
 
-def get_one_batch_of_trials(ax: AxClient, num_parallel_trials: int) -> BatchOfTrial:
+def get_one_batch_of_trials(ax: AxClient, num_parallel_trials: int) -> BatchOfTrialType:
     """Produce a batch of trials that can be run in parallel"""
     batch_of_trials = []
     for _ in range(num_parallel_trials):
@@ -77,11 +100,13 @@ def get_one_batch_of_trials(ax: AxClient, num_parallel_trials: int) -> BatchOfTr
 
 def get_batches_of_trials_from_ax(
     ax: AxClient, num_max_trials: int
-) -> List[BatchOfTrial]:
-    """Return a list of batch of trials that can be run in parallel"""
+) -> List[BatchOfTrialType]:
+    """Return a list of batch of trials.
+    Each batch of trials is a list where each trial in a batch can be run in parallel.
+    Different batches (of trials) should be executed sequentially, in the
+    order they are provided."""
     list_of_batch_of_trials = []
     recommended_max_parallelism = ax.get_recommended_max_parallelism()
-    print(recommended_max_parallelism)
     num_trials_left = num_max_trials
     for parallelism in recommended_max_parallelism:
         for batch_of_trials in get_batches_of_trials_given_parallelism(
@@ -101,14 +126,18 @@ class EarlyStopper:
     is considered to have improved when the change is more than a given
     threshold (epsilon)."""
 
-    def __init__(self, max_epochs_without_improvement, epsilon, minimize):
+    def __init__(
+        self, max_epochs_without_improvement: int, epsilon: float, minimize: bool
+    ):
         self.max_epochs_without_improvement = max_epochs_without_improvement
         self.epsilon = epsilon
         self.minimize = minimize
-        self.current_best_value = None
+        self.current_best_value: Optional[float] = None
         self.current_epochs_without_improvement = 0
 
-    def should_stop(self, potential_best_value, best_parameters):
+    def should_stop(
+        self, potential_best_value: float, best_parameters: BestParameterType
+    ) -> bool:
         """Check if the optimisation process should be stopped."""
         is_improving = True
         if self.current_best_value is not None:
@@ -147,17 +176,17 @@ class EarlyStopper:
         return False
 
 
-class AxSweeper(Sweeper):
+class AxSweeper(Sweeper):  # type: ignore
     """Class to interface with the Ax Platform"""
 
     def __init__(
         self,
-        verbose_logging,
-        experiment,
-        early_stop,
-        random_seed,
-        max_trials,
-        ax_params,
+        verbose_logging: bool,
+        experiment: TaskFunction,
+        early_stop: DictConfig,
+        random_seed: int,
+        max_trials: int,
+        ax_params: DictConfig,
     ):
         self.launcher = None
         self.job_results = None
@@ -168,12 +197,17 @@ class AxSweeper(Sweeper):
         self.max_trials = max_trials
         self.ax_params = ax_params
 
-    def setup(self, config, config_loader, task_function):
+    def setup(
+        self,
+        config: DictConfig,
+        config_loader: ConfigLoader,
+        task_function: TaskFunction,
+    ) -> None:
         self.launcher = Plugins.instantiate_launcher(
             config=config, config_loader=config_loader, task_function=task_function
         )
 
-    def sweep(self, arguments):
+    def sweep(self, arguments: List[str]) -> None:
         ax_client = self.setup_ax_client(arguments)
 
         for batch_of_trials in get_batches_of_trials_from_ax(
@@ -183,7 +217,7 @@ class AxSweeper(Sweeper):
 
             overrides = [x["overrides"] for x in batch_of_trials]
 
-            rets = self.launcher.launch(overrides)
+            rets = self.launcher.launch(overrides)  # type: ignore
             for idx in range(len(batch_of_trials)):
                 val = rets[idx].return_value
                 ax_client.complete_trial(
@@ -197,19 +231,19 @@ class AxSweeper(Sweeper):
 
         log.info("Best parameters: " + str(best))
 
-    def setup_ax_client(self, arguments) -> AxClient:
+    def setup_ax_client(self, arguments: List[str]) -> AxClient:
         """Method to setup the Ax Client"""
 
         parameters = []
         for key, value in self.ax_params.items():
             parameters.append(OmegaConf.to_container(value, resolve=True))
-            parameters[-1]["name"] = key
+            parameters[-1]["name"] = key  # type: ignore
         commandline_params = self.parse_commandline_args(arguments)
         for cmd_param in commandline_params:
             for param in parameters:
-                if param["name"] == cmd_param["name"]:
+                if param["name"] == cmd_param["name"]:  # type: ignore
                     for key, value in cmd_param.items():
-                        param[key] = value
+                        param[key] = value  # type: ignore
         ax_client = AxClient(
             verbose_logging=self.verbose_logging, random_seed=self.random_seed
         )
@@ -217,10 +251,12 @@ class AxSweeper(Sweeper):
 
         return ax_client
 
-    def parse_commandline_args(self, arguments):
+    def parse_commandline_args(
+        self, arguments: List[str]
+    ) -> List[Dict[str, Union[ax_types.TParamValue, List[ax_types.TParamValue]]]]:
         """Method to parse the command line arguments and convert them into Ax parameters"""
 
-        def _is_int(string_inp):
+        def _is_int(string_inp: str) -> bool:
             """Method to check if the given string input can be parsed as integer"""
             try:
                 int(string_inp)
@@ -228,7 +264,7 @@ class AxSweeper(Sweeper):
             except ValueError:
                 return False
 
-        def _is_float(string_inp):
+        def _is_float(string_inp: str) -> bool:
             """Method to check if the given string input can be parsed as a float"""
             try:
                 float(string_inp)
@@ -245,7 +281,7 @@ class AxSweeper(Sweeper):
                 value_choices = [x for x in value.split(",")]
                 if _is_float(value_choices[0]):
                     parameter_type = ParameterType.FLOAT
-                    value_choices = [float(x) for x in value_choices]
+                    value_choices = [float(x) for x in value_choices]  # type: ignore
                 else:
                     parameter_type = ParameterType.STRING
                 parameters.append(
@@ -260,12 +296,12 @@ class AxSweeper(Sweeper):
                 # This is a Range Parameter.
                 range_start, range_end = value.split(":")
                 if _is_int(range_start) and _is_int(range_end):
-                    range_start = int(range_start)
-                    range_end = int(range_end)
+                    range_start = int(range_start)  # type: ignore
+                    range_end = int(range_end)  # type: ignore
                     param_type = ParameterType.INT
                 else:
-                    range_start = float(range_start)
-                    range_end = float(range_end)
+                    range_start = float(range_start)  # type: ignore
+                    range_end = float(range_end)  # type: ignore
                     param_type = ParameterType.FLOAT
                 parameters.append(
                     {
