d934529e8b7d068c30c5bd6c263956ff863e5419
Jeremy Rapin <jrapin@fb.com>
2020-02-20 19:00:07 +0100

    More docstrings

--- plugins/nevergrad_minimizer/example/dummy_training.py
+++ plugins/nevergrad_minimizer/example/dummy_training.py
@@ -8,10 +8,14 @@ log = logging.getLogger(__name__)
 
 @hydra.main(config_path="config.yaml")
 def dummy_training(cfg):
+    """A dummy function to minimize
+    Minimum is 0.0 at:
+    lr = 0.12, dropout=0.33, db=mnist, batch_size=4
+    """
     print(cfg.pretty())
     do = cfg.dropout
     bs = cfg.batch_size
-    out = abs(do - 0.33) + int(cfg.db == "mnist") + abs(cfg.lr - 0.12) + bs
+    out = abs(do - 0.33) + int(cfg.db == "mnist") + abs(cfg.lr - 0.12) + abs(bs - 4)
     logging.info("dummy_training(dropout=%s, lr=%s, db=%s, batch_size=%s) = %s",
                  do, cfg.lr, cfg.db, bs, out)
     return out
