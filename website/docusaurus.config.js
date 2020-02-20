711c589a3f6aee4cc9899aa05ecdea8ce1bc9eb6
3Jane <55730900+3janeAI@users.noreply.github.com>
2020-02-20 12:01:43 -0800

    [website] add versions page (#431)

--- website/docusaurus.config.js
+++ website/docusaurus.config.js
@@ -30,6 +30,7 @@ module.exports = {
             },
             links: [
                 {to: 'docs/intro', label: 'Docs', position: 'left'},
+                {to: 'versions', label: 'Versions', position: 'left'},
                 {to: 'https://github.com/facebookresearch/hydra', label: 'Hydra@GitHub', position: 'left'},
             ],
         },
---
+++ website/src/pages/versions.js
@@ -0,0 +1,47 @@
+import React from 'react';
+import Link from '@docusaurus/Link';
+import Layout from '@theme/Layout';
+import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
+import useBaseUrl from '@docusaurus/useBaseUrl';
+
+import versions from '../../versions.json';
+
+function Versions() {
+  const context = useDocusaurusContext();
+  const {siteConfig = {}} = context;
+  const latestVersion = versions[0];
+
+  return (
+    <Layout permalink="/versions">
+      <div className="container margin-vert--xl">
+       <h1>{siteConfig.title} Versions</h1>
+        <div className="margin-bottom--lg">
+          <h3 id="latest">Current version (Stable)</h3>
+          <ul>
+      <li><b>{latestVersion}</b> <Link to={useBaseUrl('/docs/intro')}>Documentation</Link></li>
+          </ul>
+          <h3 id="rc">Latest Version</h3>
+          <p>Here you can find the latest documentation and unreleased code.</p>
+          <ul>
+      <li><b>Next Release</b> <Link to={useBaseUrl('/docs/next/intro')}>Documentation</Link></li>
+          </ul>
+          <h3 id="archive">Past Versions</h3>
+          <p>
+            Here you can find documentation for previous versions of Docusaurus.
+          </p>
+          <ul>
+              {versions.map(
+                version =>
+                  version !== latestVersion && (
+                      <li>{version} <Link to={useBaseUrl(`/docs/${version}/intro`)}>Documentation</Link></li>))}
+           </ul>
+         </div>
+      </div>
+   </Layout>
+  );
+};
+
+
+Versions.title = 'Versions';
+export default Versions;
+
