ea98c79fc9abd6e89dc8ad858f884842f596ab55
Omry Yadan <omry@fb.com>
2020-02-24 18:08:02 -0800

    versions selector cleanup

--- website/src/pages/versions.js
+++ website/src/pages/versions.js
@@ -8,38 +8,32 @@ import useBaseUrl from '@docusaurus/useBaseUrl';
 import versions from '../../versions.json';
 
 function Versions() {
-  const context = useDocusaurusContext();
-  const {siteConfig = {}} = context;
-  const latestVersion = versions[0];
+    const context = useDocusaurusContext();
+    const {siteConfig = {}} = context;
+    const latestVersion = versions[0];
 
-  return (
-    <Layout permalink="/versions">
-      <div className="container margin-vert--xl">
-       <h1>{siteConfig.title} Versions</h1>
-        <div className="margin-bottom--lg">
-          <h3 id="latest">Current version (Stable)</h3>
-          <ul>
-      <li><b>{latestVersion}</b> <Link to={useBaseUrl('/docs/intro')}>Documentation</Link></li>
-          </ul>
-          <h3 id="rc">Latest Version</h3>
-          <p>Here you can find the latest documentation and unreleased code.</p>
-          <ul>
-      <li><b>Next Release</b> <Link to={useBaseUrl('/docs/next/intro')}>Documentation</Link></li>
-          </ul>
-          <h3 id="archive">Past Versions</h3>
-          <p>
-            Here you can find documentation for previous versions of Hydra.
-          </p>
-          <ul>
-              {versions.map(
-                version =>
-                  version !== latestVersion && (
-                      <li>{version} <Link to={useBaseUrl(`/docs/${version}/intro`)}>Documentation</Link></li>))}
-           </ul>
-         </div>
-      </div>
-   </Layout>
-  );
+    return (
+        <Layout permalink="/versions">
+            <div className="container margin-vert--xl">
+                <h1>{siteConfig.title} Versions</h1>
+                <div className="margin-bottom--lg">
+                    <ul>
+                        <li><Link to={useBaseUrl(`/docs/intro`)}>Stable version : (<b>{latestVersion}</b>)</Link></li>
+                        <li><Link to={useBaseUrl('/docs/next/intro')}>Next version (<b>master</b>)</Link></li>
+                    </ul>
+                    {versions.length > 1 && <h3 id="archive">Past Versions</h3>}
+                    {<ul>{
+                        versions.filter(function (v) {return v != latestVersion}).map(
+                            version => (
+                                <li>{version} <Link to={useBaseUrl(`/docs/${version}/intro`)}>Documentation</Link></li>)
+                        )
+                    }
+                    </ul>
+                    }
+                </div>
+            </div>
+        </Layout>
+    );
 };
 
 
