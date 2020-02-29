c9a1a44f25dbdcccda66e3a8976fdf3d177c5435
Shagun Sodhani <sshagunsodhani@gmail.com>
2020-02-28 22:03:24 -0500

    Include python3.8 for Ax plugin

--- plugins/hydra_ax_sweeper/setup.py
+++ plugins/hydra_ax_sweeper/setup.py
@@ -17,6 +17,7 @@ with open("README.md", "r") as fh:
             "License :: OSI Approved :: MIT License",
             "Programming Language :: Python :: 3.6",
             "Programming Language :: Python :: 3.7",
+            "Programming Language :: Python :: 3.8",
             "Operating System :: POSIX :: Linux",
         ],
         install_requires=["hydra-core>=1.0.0-rc1", "ax-platform[mysql]==0.1.9"],
