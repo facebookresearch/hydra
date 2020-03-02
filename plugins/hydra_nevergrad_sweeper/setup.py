d01eb335906785dd4ffbec5897ddd3c898daaba7
Jeremy Rapin <jrapin@fb.com>
2020-03-02 14:38:19 +0100

    Remove comments

--- plugins/hydra_nevergrad_sweeper/setup.py
+++ plugins/hydra_nevergrad_sweeper/setup.py
@@ -14,18 +14,12 @@ with open("README.md", "r") as fh:
         url="https://github.com/facebookresearch/hydra/",
         packages=find_packages(exclude=["tests", "example"]),
         classifiers=[
-            # Feel free to choose another license
             "License :: OSI Approved :: MIT License",
-            # Python versions are used by the noxfile in Hydra to determine which
-            # python versions to test this plugin with
             "Programming Language :: Python :: 3.6",
             "Programming Language :: Python :: 3.7",
             "Programming Language :: Python :: 3.8",
             "Operating System :: OS Independent",
         ],
         install_requires=["hydra-core>=1.0.0rc1", "nevergrad>=0.3.2"],
-        # If this plugin is providing configuration files, be sure to include them in the package.
-        # See MANIFEST.in.
-        # For configurations to be discoverable at runtime, they should also be added to the search path.
         include_package_data=True,
     )
