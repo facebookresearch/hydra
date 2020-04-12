54ba12d26fa0abb792f2d1931f635416fa28801a
Omry Yadan <omry@fb.com>
2020-04-12 14:25:09 -0700

    new lint_plugins nox command

--- plugins/examples/example_launcher_plugin/tests/test_example_launcher_plugin.py
+++ plugins/examples/example_launcher_plugin/tests/test_example_launcher_plugin.py
@@ -1,5 +1,5 @@
 # Copyright (c) Facebook, Inc. and its affiliates. All Rights Reserved
-import pytest
+import pytest  # type: ignore
 
 from hydra.core.plugins import Plugins
 from hydra.plugins.launcher import Launcher
