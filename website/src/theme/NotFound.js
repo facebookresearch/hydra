ad7490aefda3b194063e63ba66f6137be0d885bc
Omry Yadan <omry@fb.com>
2020-05-27 02:10:34 -0700

    const

--- website/src/theme/NotFound.js
+++ website/src/theme/NotFound.js
@@ -9,7 +9,7 @@ import Layout from '@theme/Layout';
 
 
 function NotFound({location}) {
-    var routing = {
+    const routing = {
         "/redirect-me" : "/still-not-found"
     }
     if (routing[location.pathname] != null){
