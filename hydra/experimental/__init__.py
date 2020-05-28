557a39ff262fb21b40f0cb13e0f884a1ba375e0a
Omry Yadan <omry@fb.com>
2020-05-28 12:58:23 -0700

    added new initialization methods to hydra.experimental exports

--- hydra/experimental/__init__.py
+++ hydra/experimental/__init__.py
@@ -1,4 +1,4 @@
 # Copyright (c) Facebook, Inc. and its affiliates. All Rights Reserved
-from .compose import compose, initialize
+from .compose import compose, initialize, initialize_with_file, initialize_with_module
 
-__all__ = ["initialize", "compose"]
+__all__ = ["initialize", "compose", "initialize_with_file", "initialize_with_module"]
