b16826c23529bd92cfd6f28e82957a6e612d79bf
Omry Yadan <omry@fb.com>
2020-06-06 17:11:10 -0700

    upgrade to ax-pltform 0.1.12

--- plugins/hydra_ax_sweeper/setup.py
+++ plugins/hydra_ax_sweeper/setup.py
@@ -16,12 +16,12 @@ with open("README.md", "r") as fh:
         packages=find_namespace_packages(include=["hydra_plugins.*"]),
         classifiers=[
             "License :: OSI Approved :: MIT License",
-            "Programming Language :: Python :: 3.6",
             "Programming Language :: Python :: 3.7",
             "Programming Language :: Python :: 3.8",
             "Operating System :: POSIX :: Linux",
+            "Operating System :: MacOS",
             "Development Status :: 4 - Beta",
         ],
-        install_requires=["hydra-core~=1.0.0rc1", "ax-platform[mysql]==0.1.9"],
+        install_requires=["hydra-core~=1.0.0rc1", "ax-platform[mysql]>=0.1.12"],
         include_package_data=True,
     )
