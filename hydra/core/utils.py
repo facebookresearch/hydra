31aeb68c6c892ba481879667264cf40ab68ae91c
Binsheng Liu <441707+binshengliu@users.noreply.github.com>
2020-06-16 13:44:24 +1000

    save conf before task execution

--- hydra/core/utils.py
+++ hydra/core/utils.py
@@ -112,6 +112,12 @@ def run_job(
         hydra_cfg = OmegaConf.masked_copy(config, "hydra")
         assert isinstance(hydra_cfg, DictConfig)
 
+        if config.hydra.output_subdir is not None:
+            hydra_output = Path(config.hydra.output_subdir)
+            _save_config(task_cfg, "config.yaml", hydra_output)
+            _save_config(hydra_cfg, "hydra.yaml", hydra_output)
+            _save_config(config.hydra.overrides.task, "overrides.yaml", hydra_output)
+
         with env_override(hydra_cfg.hydra.job.env_set):
             ret.return_value = task_function(task_cfg)
         ret.task_name = JobRuntime.instance().get("name")
@@ -119,12 +125,6 @@ def run_job(
         # shut down logging to ensure job log files are closed.
         # If logging is still required after run_job caller is responsible to re-initialize it.
         logging.shutdown()
-
-        if config.hydra.output_subdir is not None:
-            hydra_output = Path(config.hydra.output_subdir)
-            _save_config(task_cfg, "config.yaml", hydra_output)
-            _save_config(hydra_cfg, "hydra.yaml", hydra_output)
-            _save_config(config.hydra.overrides.task, "overrides.yaml", hydra_output)
 
         return ret
     finally:
