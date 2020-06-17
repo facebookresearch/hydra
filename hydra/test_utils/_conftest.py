734ae66049f328277e5fd3a97eea3f09278473fc
Omry Yadan <omry@fb.com>
2020-06-17 12:40:50 -0700

    removed conftests from plugins and hydra tests

--- hydra/test_utils/_conftest.py
+++
@@ -1,8 +0,0 @@
-# Copyright (c) Facebook, Inc. and its affiliates. All Rights Reserved
-import warnings
-
-warnings.warn(
-    message="Importing hydra.test_utils._conftest is no longer needed."
-    " Install hydra_pytest_plugin to gain access to the standard Hydra pytest fixtures",
-    category=UserWarning,
-)
--- plugins/examples/example_launcher_plugin/tests/conftest.py
+++
@@ -1,3 +0,0 @@
-# Copyright (c) Facebook, Inc. and its affiliates. All Rights Reserved
-# Add Hydra standard test fixtures
-from hydra.test_utils._conftest import *  # noqa F401
