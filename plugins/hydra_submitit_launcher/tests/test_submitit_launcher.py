37e4bf5186f14bd07f58cae51451aaa924212ee7
Jeremy Rapin <jrapin@fb.com>
2020-06-19 16:29:25 +0200

    black

--- plugins/hydra_submitit_launcher/tests/test_submitit_launcher.py
+++ plugins/hydra_submitit_launcher/tests/test_submitit_launcher.py
@@ -16,16 +16,12 @@ chdir_plugin_root()
 
 
 @pytest.mark.parametrize(
-    "cls", [
-        submitit_launcher.LocalSubmititLauncher,
-        submitit_launcher.SlurmSubmititLauncher,
-    ]
+    "cls",
+    [submitit_launcher.LocalSubmititLauncher, submitit_launcher.SlurmSubmititLauncher],
 )
 def test_discovery(cls: Type[Launcher]) -> None:
     # Tests that this plugin can be discovered via the plugins subsystem when looking for Launchers
-    assert cls.__name__ in [
-        x.__name__ for x in Plugins.instance().discover(Launcher)
-    ]
+    assert cls.__name__ in [x.__name__ for x in Plugins.instance().discover(Launcher)]
 
 
 @pytest.mark.parametrize(
