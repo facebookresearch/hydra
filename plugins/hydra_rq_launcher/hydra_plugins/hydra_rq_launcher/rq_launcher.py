6d391a317de14cc74482ddae0f472e226dfbea54
jan-matthis <mail@jan-matthis.de>
2020-06-22 23:37:20 +0200

    Add hydra_rq_launcher with feedback (#683)

---
+++ plugins/hydra_rq_launcher/hydra_plugins/hydra_rq_launcher/rq_launcher.py
@@ -0,0 +1,46 @@
+# Copyright (c) Facebook, Inc. and its affiliates. All Rights Reserved
+import logging
+from typing import Any, Optional, Sequence
+
+from hydra.core.config_loader import ConfigLoader
+from hydra.core.utils import JobReturn
+from hydra.plugins.launcher import Launcher
+from hydra.types import TaskFunction
+from omegaconf import DictConfig, OmegaConf
+
+from .config import RQConf
+
+log = logging.getLogger(__name__)
+
+
+class RQLauncher(Launcher):
+    def __init__(self, **params: Any) -> None:
+        """RQ Launcher
+
+        Launches jobs using RQ (Redis Queue). For details, refer to:
+        https://python-rq.org
+        """
+        self.config: Optional[DictConfig] = None
+        self.config_loader: Optional[ConfigLoader] = None
+        self.task_function: Optional[TaskFunction] = None
+
+        self.rq = OmegaConf.structured(RQConf(**params))
+
+    def setup(
+        self,
+        config: DictConfig,
+        config_loader: ConfigLoader,
+        task_function: TaskFunction,
+    ) -> None:
+        self.config = config
+        self.config_loader = config_loader
+        self.task_function = task_function
+
+    def launch(
+        self, job_overrides: Sequence[Sequence[str]], initial_job_idx: int
+    ) -> Sequence[JobReturn]:
+        from . import _core
+
+        return _core.launch(
+            launcher=self, job_overrides=job_overrides, initial_job_idx=initial_job_idx
+        )
---
+++ plugins/hydra_rq_launcher/setup.py
@@ -0,0 +1,27 @@
+# Copyright (c) Facebook, Inc. and its affiliates. All Rights Reserved
+# type: ignore
+from setuptools import find_namespace_packages, setup
+
+with open("README.md", "r") as fh:
+    LONG_DESC = fh.read()
+    setup(
+        name="hydra-rq-launcher",
+        version="1.0.1",
+        author="Jan-Matthis Lueckmann, Omry Yadan",
+        author_email="mail@jan-matthis.de, omry@fb.com",
+        description="Redis Queue (RQ) Launcher for Hydra apps",
+        long_description=LONG_DESC,
+        long_description_content_type="text/markdown",
+        url="https://github.com/facebookresearch/hydra/",
+        packages=find_namespace_packages(include=["hydra_plugins.*"]),
+        classifiers=[
+            "License :: OSI Approved :: MIT License",
+            "Programming Language :: Python :: 3.6",
+            "Programming Language :: Python :: 3.7",
+            "Programming Language :: Python :: 3.8",
+            "Operating System :: MacOS",
+            "Operating System :: POSIX :: Linux",
+        ],
+        install_requires=["cloudpickle", "fakeredis", "hydra-core~=1.0.0rc1", "rq"],
+        include_package_data=True,
+    )
