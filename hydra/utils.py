30974a7f9308b92bba704125c3ffcf5252831f46
Shagun Sodhani <sodhani@fb.com>
2020-07-14 15:21:56 -0700

    Add support for instantiating/calling None config

--- hydra/utils.py
+++ hydra/utils.py
@@ -4,7 +4,7 @@ import os
 from pathlib import Path
 from typing import Any, Callable, Union
 
-from omegaconf import DictConfig
+from omegaconf import DictConfig, OmegaConf
 
 from hydra._internal.utils import (
     _call_callable,
@@ -26,6 +26,8 @@ def call(config: Union[ObjectConf, DictConfig], *args: Any, **kwargs: Any) -> An
     :param kwargs: optional named parameters pass-through
     :return: the return value from the specified class or method
     """
+    if OmegaConf.is_none(config):
+        return None
     try:
         cls = _get_cls_name(config)
         type_or_callable = _locate(cls)
