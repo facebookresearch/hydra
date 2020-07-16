033c9182d8c80c9b6ca8d0108066b423f1def734
Omry Yadan <omry@fb.com>
2020-07-16 11:15:36 -0700

    fixed merging of defaults list missing in Structured Config and populated from file

---
+++ tests/test_apps/defaults_in_schema_missing/my_app.py
@@ -0,0 +1,43 @@
+# Copyright (c) Facebook, Inc. and its affiliates. All Rights Reserved
+from dataclasses import dataclass
+from typing import Any, List
+
+from omegaconf import MISSING, DictConfig
+
+import hydra
+from hydra.core.config_store import ConfigStore
+
+
+@dataclass
+class DBConfig:
+    driver: str = MISSING
+    host: str = "localhost"
+    port: int = MISSING
+
+
+@dataclass
+class MySQLConfig(DBConfig):
+    driver: str = "mysql"
+    port: int = 3306
+    user: str = MISSING
+    password: str = MISSING
+
+
+@dataclass
+class Config:
+    defaults: List[Any] = MISSING
+    db: DBConfig = MISSING
+
+
+cs = ConfigStore.instance()
+cs.store(group="db", name="mysql", node=MySQLConfig, provider="main")
+cs.store(name="config", node=Config, provider="main")
+
+
+@hydra.main(config_path="conf", config_name="config")
+def my_app(cfg: DictConfig) -> None:
+    print(cfg.pretty())
+
+
+if __name__ == "__main__":
+    my_app()
