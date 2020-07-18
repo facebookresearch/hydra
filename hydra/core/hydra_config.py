2e76cbbc8b9dde5fd1c2968f7a0115e3764cbf8c
Omry Yadan <omry@fb.com>
2020-07-17 19:48:17 -0700

    771 override dirname bug (#792)
    * Allow interpolation in the Hydra config node to access the application config node
    * Lint

--- hydra/core/hydra_config.py
+++ hydra/core/hydra_config.py
@@ -15,7 +15,13 @@ class HydraConfig(metaclass=Singleton):
         assert cfg is not None
         OmegaConf.set_readonly(cfg.hydra, True)
         assert OmegaConf.get_type(cfg, "hydra") == HydraConf
+        # THis is emulating a node that is hidden.
+        # It's quiet a hack but it will be much better once
+        # https://github.com/omry/omegaconf/issues/280 is done
+        # The motivation is that this allows for interpolations from the hydra node
+        # into the user's config.
         self.cfg = OmegaConf.masked_copy(cfg, "hydra")  # type: ignore
+        self.cfg.hydra._set_parent(cfg)  # type: ignore
 
     @staticmethod
     def get() -> HydraConf:
---
+++ tests/test_apps/hydra_to_cfg_interpolation/my_app.py
@@ -0,0 +1,13 @@
+# Copyright (c) Facebook, Inc. and its affiliates. All Rights Reserved
+from omegaconf import DictConfig
+
+import hydra
+
+
+@hydra.main(config_name="config")
+def my_app(cfg: DictConfig) -> None:
+    print(cfg.c)
+
+
+if __name__ == "__main__":
+    my_app()
