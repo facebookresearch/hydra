baf22887a4b24d1a2d8527f4d21a2f7ad87cbbb0
Omry Yadan <omry@fb.com>
2020-07-18 17:52:50 -0700

    bump repo version

--- hydra/__init__.py
+++ hydra/__init__.py
@@ -1,10 +1,10 @@
 # Copyright (c) Facebook, Inc. and its affiliates. All Rights Reserved
+
+# Source of truth for Hydra's version
+__version__ = "1.0.0rc3"
 from hydra import utils
 from hydra.errors import MissingConfigException
 from hydra.main import main
 from hydra.types import TaskFunction
-
-# Source of truth for Hydra's version
-__version__ = "1.0.0rc2"
 
 __all__ = ["__version__", "MissingConfigException", "main", "utils", "TaskFunction"]
