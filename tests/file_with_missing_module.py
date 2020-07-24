2e9408dee4664f75b73760d7043ed3ded1530fb0
Omry Yadan <omry@fb.com>
2020-07-24 14:21:25 -0700

    Update file_with_missing_module.py

--- tests/file_with_missing_module.py
+++ tests/file_with_missing_module.py
@@ -1,4 +1,7 @@
 # Copyright (c) Facebook, Inc. and its affiliates. All Rights Reserved
+
+# Used for testing that instantiation is failing with the right error
+# in case of a missing imported module
 import some_missing_module  # type: ignore # noqa: F401
 
 
