b850d0911b4433cffe20233fe413f98eff11a351
Shagun Sodhani <sodhani@fb.com>
2020-07-24 16:21:28 -0400

    Add a file used by the testcases:

---
+++ tests/file_with_missing_module.py
@@ -0,0 +1,7 @@
+# Copyright (c) Facebook, Inc. and its affiliates. All Rights Reserved
+import some_missing_module  # type: ignore # noqa: F401
+
+
+class AClass:
+    def __init__(self) -> None:
+        self.x = 1
