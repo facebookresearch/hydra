9a9aff7f777c4aef35623487515a3341507664dc
Jieru Hu <jieru@fb.com>
2020-08-06 13:21:33 -0700

    test out the script

--- tools/circle_tests.py
+++ tools/circle_tests.py
@@ -1,16 +1,25 @@
+import json
+import re
+import sys
+
 import requests
 import os
 
-os.environ['CIRCLECI_TOKEN'] = "87c43392dbf2fa64d10c3d35d2c933ba94694e68"
-
 auth = os.environ.get("CIRCLECI_TOKEN", "0")
 
-def run():
-    r = requests.post("https://circleci.com/api/v2/project/gh/facebookresearch/hydra/pipeline", headers={"Circle-Token": auth}, data={"branch": "master"})
+git_repo_pattern = "((git|ssh|http(s)?)|(git@[\w\.]+))(:(//)?)([\w\.@\:/\-~]+)(\.git)(/)?"
+
+def run(branch: str, git_repo: str) -> None:
+    assert auth != "0"
+    p = re.compile(git_repo_pattern)
+    m = re.search(p, git_repo)
+    repo_name = m.group(m.groups().index(".git"))
+    headers = {"Circle-Token": auth, 'Content-type': 'application/json'}
+    data = {"branch": branch, "parameters": {"plugin": "hydra_colorlog", "plugin_test": True}}
+    r = requests.post(f"https://circleci.com/api/v2/project/gh/{repo_name}/pipeline", headers=headers, data=json.dumps(data))
     print(r.json())
-    r = requests.get("https://circleci.com/api/v2/me", headers={"Circle-Token": auth})
-    print(r.json())
+
 
 
 if __name__ == '__main__':
-    run()
+    run(sys.argv[1], sys.argv[2])
