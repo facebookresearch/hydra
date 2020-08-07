0045ff19be1a744df5a62558ed9e6086bfacf1c5
Jieru Hu <jieru@fb.com>
2020-08-06 17:15:19 -0700

    fix lints

--- tools/circleci_pipeline.py
+++ tools/circleci_pipeline.py
@@ -2,36 +2,31 @@
 # type: ignore
 import json
 import os
-from os.path import dirname
 import re
 import sys
-from typing import List, Dict
+from os.path import dirname
+from typing import List
 
 import requests
-
 
 auth = os.environ.get("CIRCLECI_TOKEN", "0")
 
 git_repo_pattern = (
-    "((git|ssh|http(s)?)|(git@[\w\.]+))(:(//)?)([\w\.@\:/\-~]+)(\.git)(/)?"
+    r"((git|ssh|http(s)?)|(git@[\w\.]+))(:(//)?)([\w\.@\:/\-~]+)(\.git)(/)?"
 )
 
 BASE = dirname(os.path.abspath(os.path.dirname(__file__)))
 
-def get_available_plugin() -> List[Dict[str, str]]:
+
+def get_available_plugin() -> List[str]:
     blacklist = [".isort.cfg"]
-    example_plugins = [
-        {"dir_name": x, "path": f"examples/{x}"}
-        for x in sorted(os.listdir(os.path.join(BASE, "plugins/examples")))
-        if x not in blacklist
-    ]
-    plugins = [
+    ps = [
         {"dir_name": x, "path": x}
         for x in sorted(os.listdir(os.path.join(BASE, "plugins")))
         if x != "examples"
         if x not in blacklist
     ]
-    return plugins + example_plugins
+    return [p["path"] for p in ps] + ["examples"]
 
 
 def run(branch: str, git_repo: str) -> None:
@@ -40,8 +35,8 @@ def run(branch: str, git_repo: str) -> None:
     m = re.search(p, git_repo)
     repo_name = m.group(m.groups().index(".git"))
     headers = {"Circle-Token": auth, "Content-type": "application/json"}
-    plugins = [p["path"] for p in get_available_plugin()]
-    for p in plugins:
+
+    for p in get_available_plugin():
         data = {"branch": branch, "parameters": {"plugin": p, "plugin_test": True}}
         r = requests.post(
             f"https://circleci.com/api/v2/project/gh/{repo_name}/pipeline",
