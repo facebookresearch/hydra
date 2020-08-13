11adf550b2d0702b501db23cb2e1494ff612a370
Jieru Hu <jieru@fb.com>
2020-08-12 17:02:21 -0700

    group 4 plugins together

--- tools/circleci_pipeline.py
+++ tools/circleci_pipeline.py
@@ -35,7 +35,7 @@ def get_available_plugin() -> List[str]:
     ]
     plugins = [p["path"] for p in ps] + ["examples"]
     random.shuffle(plugins)
-    return [",".join(w) for w in list(chunk(plugins, 3))]
+    return [",".join(w) for w in list(chunk(plugins, 4))]
 
 
 def run(branch: str, git_repo: str) -> None:
