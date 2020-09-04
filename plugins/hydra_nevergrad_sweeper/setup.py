15eab46c8003607953a4625fcd301bcb2de53e63
Omry Yadan <omry@fb.com>
2020-09-04 14:10:59 -0700

    python tools/release/release.py action=bump

--- plugins/hydra_nevergrad_sweeper/setup.py
+++ plugins/hydra_nevergrad_sweeper/setup.py
@@ -6,7 +6,7 @@ with open("README.md", "r") as fh:
     LONG_DESC = fh.read()
     setup(
         name="hydra-nevergrad-sweeper",
-        version="1.0.0rc5",
+        version="1.0.0rc6",
         author="Jeremy Rapin, Omry Yadan",
         author_email="jrapin@fb.com, omry@fb.com",
         description="Hydra Nevergrad Sweeper plugin",
