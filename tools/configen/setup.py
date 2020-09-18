1f71d43153ce04392e5579ef3ab0992a7dd26e06
Omry Yadan <omry@fb.com>
2020-09-17 17:03:34 -0700

    bump versions

--- tools/configen/setup.py
+++ tools/configen/setup.py
@@ -6,7 +6,7 @@ from setuptools import find_packages, setup
 
 setup(
     name="hydra-configen",
-    version="0.9.0dev4",
+    version="0.9.0dev5",
     packages=find_packages(include=["configen"]),
     entry_points={"console_scripts": ["configen = configen.configen:main"]},
     author="Omry Yadan",
