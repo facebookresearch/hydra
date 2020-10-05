b686b1de6a970209b7cebf6f904e4f977a59ba3b
Jieru Hu <jieru.hu@gmail.com>
2020-10-05 12:43:00 -0700

    add logging for temp path

--- plugins/hydra_ray_launcher/hydra_plugins/hydra_ray_launcher/_launcher_util.py
+++ plugins/hydra_ray_launcher/hydra_plugins/hydra_ray_launcher/_launcher_util.py
@@ -89,8 +89,9 @@ def ray_tmp_dir(yaml_path: str, docker: bool) -> Generator[Any, None, None]:
     mktemp_args = args + [yaml_path, "echo $(mktemp -d)"]
     out, _ = _run_command(mktemp_args)
     tmppath = [x for x in out.strip().split() if x.startswith("/tmp/")]  # nosec
-    assert len(tmppath) == 1
+    assert len(tmppath) == 1, f"tmppath is : {tmppath}"
     tmp_path = tmppath[0]
+    log.info(f"Created temp path on remote server {tmp_path}")
     yield tmp_path
     rmtemp_args = args + [yaml_path, f"rm -rf {tmp_path}"]
     _run_command(rmtemp_args)
