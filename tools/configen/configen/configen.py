9e5b679f2780f30525ea73e3b367a0576c420859
Tomasz Kornuta <tkornuta@nvidia.com>
2020-10-20 13:56:51 -0700

    2020, <org> headersD
    Signed-off-by: Tomasz Kornuta <tkornuta@nvidia.com>

--- tools/configen/configen/configen.py
+++ tools/configen/configen/configen.py
@@ -1,4 +1,4 @@
-# Copyright (c) 2020 Facebook, Inc. and its affiliates. All Rights Reserved
+# Copyright (c) 2020, Facebook, Inc. and its affiliates. All Rights Reserved
 # SPDX-License-Identifier: MIT
 
 import inspect
