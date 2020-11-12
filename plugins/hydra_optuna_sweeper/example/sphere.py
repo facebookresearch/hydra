bfda74f2e51e3d3cdca31cedfd2a8020a3d167e6
Toshihiko Yanase <toshihiko.yanase@gmail.com>
2020-11-12 14:46:30 +0900

    Remove unused variable and fix type hinting.

--- plugins/hydra_optuna_sweeper/example/sphere.py
+++ plugins/hydra_optuna_sweeper/example/sphere.py
@@ -1,17 +1,12 @@
 # Copyright (c) Facebook, Inc. and its affiliates. All Rights Reserved
-import logging
-from typing import Any
-
 import hydra
 from omegaconf import DictConfig
 
-logger = logging.getLogger(__name__)
-
 
 @hydra.main(config_path="conf", config_name="config")
-def sphere(cfg: DictConfig) -> Any:
-    x = cfg.x
-    y = cfg.y
+def sphere(cfg: DictConfig) -> float:
+    x:float = cfg.x
+    y:float = cfg.y
     return x ** 2 + y ** 2
 
 
