b720377ea01f57561aa6b80ac4f730c60c9cfff3
Emile Mathieu <emile.mathieu@stats.ox.ac.uk>
2020-11-12 20:55:18 +0000

    Can now specify the maximum number of jobs running in parallel

--- plugins/hydra_submitit_launcher/hydra_plugins/hydra_submitit_launcher/config.py
+++ plugins/hydra_submitit_launcher/hydra_plugins/hydra_submitit_launcher/config.py
@@ -31,9 +31,7 @@ class BaseQueueConf:
 class SlurmQueueConf(BaseQueueConf):
     """Slurm configuration overrides and specific parameters"""
 
-    _target_: str = (
-        "hydra_plugins.hydra_submitit_launcher.submitit_launcher.SlurmLauncher"
-    )
+    _target_: str = ("hydra_plugins.hydra_submitit_launcher.submitit_launcher.SlurmLauncher")
 
     # Params are used to configure sbatch, for more info check:
     # https://github.com/facebookincubator/submitit/blob/master/submitit/slurm/slurm.py
@@ -60,27 +58,21 @@ class SlurmQueueConf(BaseQueueConf):
     # Useful to add parameters which are not currently available in the plugin.
     # Eg: {"mail-user": "blublu@fb.com", "mail-type": "BEGIN"}
     additional_parameters: Dict[str, Any] = field(default_factory=dict)
+    # Maximum number of jobs running in parallel
+    array_parallelism: int = 256
 
 
 @dataclass
 class LocalQueueConf(BaseQueueConf):
-    _target_: str = (
-        "hydra_plugins.hydra_submitit_launcher.submitit_launcher.LocalLauncher"
-    )
+    _target_: str = ("hydra_plugins.hydra_submitit_launcher.submitit_launcher.LocalLauncher")
 
 
 # finally, register two different choices:
 ConfigStore.instance().store(
-    group="hydra/launcher",
-    name="submitit_local",
-    node=LocalQueueConf(),
-    provider="submitit_launcher",
+    group="hydra/launcher", name="submitit_local", node=LocalQueueConf(), provider="submitit_launcher",
 )
 
 
 ConfigStore.instance().store(
-    group="hydra/launcher",
-    name="submitit_slurm",
-    node=SlurmQueueConf(),
-    provider="submitit_launcher",
+    group="hydra/launcher", name="submitit_slurm", node=SlurmQueueConf(), provider="submitit_launcher",
 )
