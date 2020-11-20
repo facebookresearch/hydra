0cf4cab8898a3c068d4d6656d37cb81b2ea1b763
Jieru Hu <jieru.hu@gmail.com>
2020-11-19 17:01:29 -0800

    Update AMI for new version of ray

--- plugins/hydra_ray_launcher/tests/test_ray_aws_launcher.py
+++ plugins/hydra_ray_launcher/tests/test_ray_aws_launcher.py
@@ -55,7 +55,7 @@ except (NoCredentialsError, NoRegionError):
     aws_not_configured = True
 
 
-ami = os.environ.get("AWS_RAY_AMI", "ami-0bb5a2431d0b0dcea")
+ami = os.environ.get("AWS_RAY_AMI", "ami-099b85625184ce698")
 security_group_id = os.environ.get("AWS_RAY_SECURITY_GROUP", "sg-0a12b09a5ff961aee")
 subnet_id = os.environ.get("AWS_RAY_SUBNET", "subnet-acd2cfe7")
 instance_role = os.environ.get(
