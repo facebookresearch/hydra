1f198381c4bc0d86d5f7ce8dc9336594edd1412a
Omry Yadan <omry@fb.com>
2020-11-23 16:35:26 -0800

    added tests for get_config_path and get_group_path

--- hydra/core/NewDefaultElement.py
+++ hydra/core/NewDefaultElement.py
@@ -24,14 +24,27 @@ class ConfigDefault(InputDefault):
         return self.path == "_self_" or self.path.endswith("/_self_")
 
     def get_group_path(self) -> str:
+        assert self.parent_base_dir is not None
         idx = self.path.rfind("/")
         if idx == -1:
-            return ""
+            group = ""
         else:
-            return self.path[0:idx]
+            group = self.path[0:idx]
+
+        if self.parent_base_dir == "":
+            return group
+        else:
+            if group == "":
+                return f"{self.parent_base_dir}"
+            else:
+                return f"{self.parent_base_dir}/{group}"
 
     def get_config_path(self) -> str:
-        return self.path
+        assert self.parent_base_dir is not None
+        if self.parent_base_dir == "":
+            return self.path
+        else:
+            return f"{self.parent_base_dir}/{self.path}"
 
 
 @dataclass
@@ -45,6 +58,9 @@ class GroupDefault(InputDefault):
 
     parent_base_dir: Optional[str] = field(default=None, compare=False, repr=False)
     config_name_overridden: bool = field(default=False, compare=False, repr=False)
+
+    def __post_init__(self):
+        assert self.group is not None and self.group != ""
 
     def is_self(self) -> bool:
         return self.name == "_self_"
