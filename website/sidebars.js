a97e25b8eb4162b3d56c598abe481186b283f026
Jieru Hu <jieru@fb.com>
2020-12-10 19:19:53 -0800

    Add the configerator config source doc. (#1190)
    * Add configerator config source doc

--- website/sidebars.js
+++ website/sidebars.js
@@ -142,6 +142,7 @@ module.exports = {
             'fb/fbcode',
             'fb/internal-fb-cluster',
             'fb/fair-cluster',
+            'fb/fbcode-configerator-config-source',
         ]),
     }
 }
