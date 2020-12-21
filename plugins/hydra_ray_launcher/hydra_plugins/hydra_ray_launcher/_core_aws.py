cab9b460dbaabf69a3a9d0ca521908a0cb171d7f
Jieru Hu <jieru@fb.com>
2020-12-21 15:21:42 -0800

    updates to ray_aws config keys, example apps and doc (#1216)

--- plugins/hydra_ray_launcher/hydra_plugins/hydra_ray_launcher/_core_aws.py
+++ plugins/hydra_ray_launcher/hydra_plugins/hydra_ray_launcher/_core_aws.py
@@ -58,11 +58,18 @@ def launch(
     assert launcher.config_loader is not None
     assert launcher.task_function is not None
 
-    setup_commands = launcher.mandatory_install.install_commands
-    setup_commands.extend(launcher.ray_cluster_cfg.setup_commands)
+    setup_commands = launcher.env_setup.commands
+    with read_write(setup_commands):
+        setup_commands.extend(
+            [
+                f"pip install {package}=={version}"
+                for package, version in launcher.env_setup.pip_packages.items()
+            ]
+        )
+        setup_commands.extend(launcher.ray_cfg.cluster.setup_commands)
 
-    with read_write(launcher.ray_cluster_cfg):
-        launcher.ray_cluster_cfg.setup_commands = setup_commands
+    with read_write(launcher.ray_cfg.cluster):
+        launcher.ray_cfg.cluster.setup_commands = setup_commands
 
     configure_log(launcher.config.hydra.hydra_logging, launcher.config.hydra.verbose)
 
@@ -93,7 +100,7 @@ def launch(
         with tempfile.NamedTemporaryFile(suffix=".yaml", delete=False) as f:
             with open(f.name, "w") as file:
                 OmegaConf.save(
-                    config=launcher.ray_cluster_cfg, f=file.name, resolve=True
+                    config=launcher.ray_cfg.cluster, f=file.name, resolve=True
                 )
             launcher.ray_yaml_path = f.name
             log.info(
@@ -181,7 +188,7 @@ def launch_jobs(
 
         if launcher.stop_cluster:
             log.info("Stopping cluster now. (stop_cluster=true)")
-            if launcher.ray_cluster_cfg.provider.cache_stopped_nodes:
+            if launcher.ray_cfg.cluster.provider.cache_stopped_nodes:
                 log.info("NOT deleting the cluster (provider.cache_stopped_nodes=true)")
             else:
                 log.info("Deleted the cluster (provider.cache_stopped_nodes=false)")
