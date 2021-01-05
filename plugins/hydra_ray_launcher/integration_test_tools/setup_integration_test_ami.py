64e92cd10aa28675f4b59841a3f43b29ebdae8c1
Jieru Hu <jieru@fb.com>
2021-01-05 15:06:46 -0800

    update AMI and pin dependency version (#1268)

--- plugins/hydra_ray_launcher/integration_test_tools/setup_integration_test_ami.py
+++ plugins/hydra_ray_launcher/integration_test_tools/setup_integration_test_ami.py
@@ -1,9 +1,14 @@
 # Copyright (c) Facebook, Inc. and its affiliates. All Rights Reserved
 import subprocess
+import sys
 from datetime import datetime
 
 dependencies = [
-    "ray>=1.0.0",
+    "boto3==1.16.48",
+    "hydra-core>=1.0.0",
+    "ray==1.1.0",
+    "cloudpickle==1.6.0",
+    "pickle5==0.0.11",
 ]
 
 
@@ -14,28 +19,15 @@ def _run_command(command: str) -> str:
     return output
 
 
-def run():
-    _run_command("conda update --all -y")
-    output = _run_command("conda search python").split("\n")
-
-    # gather all the python versions and install conda envs
-    versions = set()
-    for o in output:
-        o = o.split()
-        if len(o) > 2 and o[0] == "python" and float(o[1][:3]) >= 3.6:
-            versions.add(o[1])
-    print(sorted(versions))
+def run(py_version):
 
     _run_command("rm /home/ubuntu/ray_bootstrap_config.yaml")
 
-    # prep conda env for all python versions
-
-    for v in sorted(versions):
-        _run_command(f"conda create -n hydra_{v} python={v} -y")
-        pip_path = f"/home/ubuntu/anaconda3/envs/hydra_{v}/bin/pip"
-        for d in dependencies:
-            _run_command(f"{pip_path} install {d}")
+    _run_command(f"conda create -n hydra_{py_version} python={py_version} -y")
+    pip_path = f"/home/ubuntu/anaconda3/envs/hydra_{py_version}/bin/pip"
+    for d in dependencies:
+        _run_command(f"{pip_path} install {d}")
 
 
 if __name__ == "__main__":
-    run()
+    run(sys.argv[1])
