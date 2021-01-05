9d67c7821eadc542e04f4865e685600474024ba0
Shagun Sodhani <sodhani@fb.com>
2021-01-04 18:29:46 -0800

    Add two tests checking for package rename

--- tests/test_examples/test_advanced_package_overrides.py
+++ tests/test_examples/test_advanced_package_overrides.py
@@ -3,11 +3,7 @@ from pathlib import Path
 
 from omegaconf import OmegaConf
 
-from hydra.test_utils.test_utils import (
-    assert_text_same,
-    chdir_hydra_root,
-    get_run_output,
-)
+from hydra.test_utils.test_utils import chdir_hydra_root, get_run_output
 
 chdir_hydra_root()
 
@@ -23,27 +19,6 @@ def test_advanced_package_override_simple(tmpdir: Path) -> None:
     }
 
 
-def test_advanced_package_override_simple_with_cli_pakcage_override(
-    tmpdir: Path,
-) -> None:
-    cmd = [
-        "examples/advanced/package_overrides/simple.py",
-        "hydra.run.dir=" + str(tmpdir),
-        "db@:source=mysql",
-    ]
-    result, err = get_run_output(cmd, allow_warnings=True)
-    assert OmegaConf.create(result) == {
-        "source": {"driver": "mysql", "user": "omry", "pass": "secret"}
-    }
-    from_line = (
-        "Support for renaming packages via the command line "
-        "is deprecated since Hydra 1.0.5 and will be removed in Hydra 1.1. "
-        "For more details, see https://github.com/facebookresearch/hydra/issues/1140."
-        "\n  warnings.warn(message=msg, category=UserWarning)"
-    )
-    assert_text_same(from_line, err.split("\n", 1)[1])
-
-
 def test_advanced_package_override_two_packages(tmpdir: Path) -> None:
     cmd = [
         "examples/advanced/package_overrides/two_packages.py",
@@ -54,23 +29,3 @@ def test_advanced_package_override_two_packages(tmpdir: Path) -> None:
         "source": {"driver": "mysql", "user": "omry", "pass": "secret"},
         "destination": {"driver": "mysql", "user": "omry", "pass": "secret"},
     }
-
-
-def test_advanced_package_override_two_packages_with_cli_override(tmpdir: Path) -> None:
-    cmd = [
-        "examples/advanced/package_overrides/two_packages.py",
-        "hydra.run.dir=" + str(tmpdir),
-        "db@destination:backup=mysql",
-    ]
-    result, err = get_run_output(cmd, allow_warnings=True)
-    assert OmegaConf.create(result) == {
-        "source": {"driver": "mysql", "user": "omry", "pass": "secret"},
-        "backup": {"driver": "mysql", "user": "omry", "pass": "secret"},
-    }
-    from_line = (
-        "Support for renaming packages via the command line "
-        "is deprecated since Hydra 1.0.5 and will be removed in Hydra 1.1. "
-        "For more details, see https://github.com/facebookresearch/hydra/issues/1140."
-        "\n  warnings.warn(message=msg, category=UserWarning)"
-    )
-    assert_text_same(from_line, err.split("\n", 1)[1])
