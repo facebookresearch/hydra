6eb780e5eae294fb461c1ce3b260b3f95c37f653
Omry Yadan <omry@fb.com>
2021-01-22 13:11:13 -0800

    Added copyright headers to some files

--- website/src/components/GithubLink.jsx
+++ website/src/components/GithubLink.jsx
@@ -1,3 +1,7 @@
+/**
+ * Copyright (c) Facebook, Inc. and its affiliates. All Rights Reserved
+ */
+
 import React from "react";
 import Link from "@docusaurus/Link";
 import useDocusaurusContext from "@docusaurus/useDocusaurusContext";
