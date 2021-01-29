51367599087e15f91b3aa3624948ff85e6f48a33
jgbos <jgoodwin@ll.mit.edu>
2021-01-29 15:41:34 -0500

    Support submitit setup parameter (#1230)
    * support submitit setup parameter
    
    * fixes key name
    
    * convert all configs to containers
    
    * removed unused import
    
    * convert params to primitive containers
    
    * news update
    
    * update news item
    
    * Update 1227.plugin
    
    Updated news entry style. see https://hydra.cc/docs/development/documentation#contents-of-a-news-entry
    
    * Update the link in news entry.
    
    Using commit-hash instead of master for the link to submitit code so that subsequent comments do not affect it.
    
    Co-authored-by: Omry Yadan <omry@fb.com>
    Co-authored-by: Shagun Sodhani <1321193+shagunsodhani@users.noreply.github.com>

--- plugins/hydra_submitit_launcher/hydra_plugins/hydra_submitit_launcher/submitit_launcher.py
+++ plugins/hydra_submitit_launcher/hydra_plugins/hydra_submitit_launcher/submitit_launcher.py
@@ -31,7 +31,12 @@ class BaseSubmititLauncher(Launcher):
     _EXECUTOR = "abstract"
 
     def __init__(self, **params: Any) -> None:
-        self.params = params
+        self.params = {}
+        for k, v in params.items():
+            if OmegaConf.is_config(v):
+                v = OmegaConf.to_container(v, resolve=True)
+            self.params[k] = v
+
         self.config: Optional[DictConfig] = None
         self.config_loader: Optional[ConfigLoader] = None
         self.task_function: Optional[TaskFunction] = None
