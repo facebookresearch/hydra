54a9de0340d797cf5e33c8c5b4f1dc5761c2a8fe
Stanton <stantsa@3c22fb4ac886.ant.amazon.com>
2021-02-18 14:46:47 -0700

    anonymize

--- plugins/hydra_ray_launcher/examples/docker/docker_app.py
+++ plugins/hydra_ray_launcher/examples/docker/docker_app.py
@@ -1,7 +1,7 @@
+# Copyright (c) Facebook, Inc. and its affiliates. All Rights Reserved
 import logging
 import time
 
-# Copyright (c) Facebook, Inc. and its affiliates. All Rights Reserved
 import hydra
 
 log = logging.getLogger(__name__)
