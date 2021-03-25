039e81d4fa93e822b5f01a92f115e28d01108cac
Omry Yadan <omry@fb.com>
2021-03-25 16:10:44 -0700

    Update example plugins hydra-core version

--- examples/plugins/example_generic_plugin/setup.py
+++ examples/plugins/example_generic_plugin/setup.py
@@ -24,8 +24,12 @@ with open("README.md", "r") as fh:
             "Programming Language :: Python :: 3.8",
             "Operating System :: OS Independent",
         ],
-        install_requires=["hydra-core>=1.0.0"],
-        # If this plugin is providing configuration files, be sure to include them in the package.
+        install_requires=[
+            # consider pinning to a specific major version of Hydra to avoid unexpected
+            # if a new major versio nof Hydra introduces introduces breaking changes for plugins.
+            # e.g: "hydra-core==1.0.*",
+            "hydra-core",
+        ],
         # See MANIFEST.in.
         # For configurations to be discoverable at runtime, they should also be added to the search path.
         include_package_data=True,
