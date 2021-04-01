640ccb64ebfe49bec8de8a3a44e48c03c32dbca0
Jieru Hu <jieru@fb.com>
2021-04-01 14:48:16 -0700

    windows

--- hydra/_internal/core_plugins/importlib_resources_config_source.py
+++ hydra/_internal/core_plugins/importlib_resources_config_source.py
@@ -1,6 +1,7 @@
 # Copyright (c) Facebook, Inc. and its affiliates. All Rights Reserved
 import os
 import sys
+import zipfile
 from typing import Any, List, Optional
 
 from omegaconf import OmegaConf
@@ -31,10 +32,14 @@ class ImportlibResourcesConfigSource(ConfigSource):
 
     def _read_config(self, res: Any) -> ConfigResult:
         try:
-            f = res.open()
-            header_text = f.read(512)
-            if isinstance(header_text, bytes):
-                header_text = header_text.decode("utf-8")
+            if sys.version_info[0:2] >= (3, 8) and isinstance(res, zipfile.Path):
+                f = res.open()
+                header_text = f.read(512)
+                if isinstance(header_text, bytes):
+                    header_text = header_text.decode("utf-8")
+            else:
+                f = res.open(encoding="utf-8")
+                header_text = f.read(512)
             header = ConfigSource._get_header_dict(header_text)
             f.seek(0)
             cfg = OmegaConf.load(f)
