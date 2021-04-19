ab65f77cfb3981babffafc451f033c82685cef8f
Jieru Hu <jieru@fb.com>
2021-04-19 13:47:11 -0700

    commit

--- examples/plugins/example_searchpath_plugin/setup.py
+++ examples/plugins/example_searchpath_plugin/setup.py
@@ -15,7 +15,7 @@ with open("README.md", "r") as fh:
         long_description_content_type="text/markdown",
         url="https://github.com/facebookresearch/hydra/",
         packages=find_namespace_packages(include=["hydra_plugins.*"])
-        + ["arbitrary_package"],
+        + find_packages(include=["arbitrary_package.*"]),
         classifiers=[
             # Feel free to use another license.
             "License :: OSI Approved :: MIT License",
