9121460bd665638bbc04d4cf4194affffeae7e88
Jieru Hu <jieru@fb.com>
2021-04-27 19:15:03 -0700

    Revert "Move Callbacks to hydra.core.utils"
    This reverts commit 262382e4ac5f2f5c1a7bb38540ffb4723cfd36ae.

--- hydra/core/utils.py
+++ hydra/core/utils.py
@@ -4,7 +4,6 @@ import logging
 import os
 import re
 import sys
-import warnings
 from contextlib import contextmanager
 from dataclasses import dataclass
 from datetime import datetime
@@ -90,6 +89,7 @@ def run_job(
     job_subdir_key: Optional[str],
     configure_logging: bool = True,
 ) -> "JobReturn":
+    from hydra.core.callbacks import Callbacks
 
     callbacks = Callbacks(config)
 
@@ -240,53 +240,6 @@ def validate_config_path(config_path: Optional[str]) -> None:
             """
             )
             raise ValueError(msg)
-
-
-class Callbacks:
-    def __init__(self, config: DictConfig) -> None:
-        self.callbacks = []
-        from hydra.utils import instantiate
-
-        for params in config.hydra.callbacks.values():
-            self.callbacks.append(instantiate(params))
-
-    def _notify(self, function_name: str, reverse: bool = False, **kwargs: Any) -> None:
-        callbacks = reversed(self.callbacks) if reverse else self.callbacks
-        for c in callbacks:
-            try:
-                getattr(c, function_name)(**kwargs)
-            except Exception as e:
-                warnings.warn(
-                    f"Callback {type(c).__name__}.{function_name} raised {type(e).__name__}: {e}"
-                )
-
-    def on_run_start(self, config: DictConfig, **kwargs: Any) -> None:
-        self._notify(function_name="on_run_start", config=config, **kwargs)
-
-    def on_run_end(self, config: DictConfig, **kwargs: Any) -> None:
-        self._notify(function_name="on_run_end", config=config, reverse=True, **kwargs)
-
-    def on_multirun_start(self, config: DictConfig, **kwargs: Any) -> None:
-        self._notify(function_name="on_multirun_start", config=config, **kwargs)
-
-    def on_multirun_end(self, config: DictConfig, **kwargs: Any) -> None:
-        self._notify(
-            function_name="on_multirun_end", reverse=True, config=config, **kwargs
-        )
-
-    def on_job_start(self, config: DictConfig, **kwargs: Any) -> None:
-        self._notify(function_name="on_job_start", config=config, **kwargs)
-
-    def on_job_end(
-        self, config: DictConfig, job_return: JobReturn, **kwargs: Any
-    ) -> None:
-        self._notify(
-            function_name="on_job_end",
-            config=config,
-            job_return=job_return,
-            reverse=True,
-            **kwargs,
-        )
 
 
 @contextmanager
