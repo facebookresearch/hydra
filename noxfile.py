e64111da32f113d7cfd7950c00dbc0afe1f28721
Jieru Hu <jieru@fb.com>
2021-06-21 17:55:40 -0700

    commit

--- noxfile.py
+++ noxfile.py
@@ -390,7 +390,7 @@ def test_plugins_in_directory(
             os.path.join(directory, plugin.path),
             "--use-feature=in-tree-build",  # https://github.com/pypa/pip/issues/7555
         ]
-        session.run(*cmd, silent=SILENT)
+        session.run(*cmd, silent=False)
         if not SILENT:
             session.run("pipdeptree", "-p", plugin.name)
 
