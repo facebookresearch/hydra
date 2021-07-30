a3a24bc8802cd900890411e86aea24fc28929f1f
Jasha10 <8935917+Jasha10@users.noreply.github.com>
2021-07-30 14:35:40 -0500

    Update hydra/_internal/deprecation_warning.py
    Co-authored-by: Omry Yadan <omry@fb.com>

--- hydra/_internal/deprecation_warning.py
+++ hydra/_internal/deprecation_warning.py
@@ -6,7 +6,7 @@ from hydra.errors import HydraDeprecationError
 
 
 def deprecation_warning(message: str, stacklevel: int = 1) -> None:
-    warnings_as_errors = os.environ.get("HYDRA_DEPRECATION_WARNINGS_AS_ERRORS", None)
+    warnings_as_errors = os.environ.get("HYDRA_DEPRECATION_WARNINGS_AS_ERRORS")
     if warnings_as_errors:
         raise HydraDeprecationError(message)
     else:
