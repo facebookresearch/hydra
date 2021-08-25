78756169a65192ea7bb199f1364e77dd95d9ce60
Jieru Hu <jieru@fb.com>
2021-08-25 14:07:21 -0700

    commit

--- plugins/hydra_ray_launcher/setup.py
+++ plugins/hydra_ray_launcher/setup.py
@@ -26,7 +26,7 @@ setup(
     install_requires=[
         "boto3==1.17.17",
         "hydra-core>=1.1.0.dev7",
-        "ray==1.6.0",
+        "ray[default]==1.6.0",
         "cloudpickle==1.6.0",
         "pickle5==0.0.11",
     ],
