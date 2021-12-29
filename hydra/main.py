c6f46bd1f7903223391c01e987769792092d66e5
Jasha <8935917+Jasha10@users.noreply.github.com>
2021-12-29 17:55:57 -0600

    In hydra.main: change default config_path from '.' to None

--- hydra/main.py
+++ hydra/main.py
@@ -1,19 +1,15 @@
 # Copyright (c) Facebook, Inc. and its affiliates. All Rights Reserved
 import functools
-from textwrap import dedent
 from typing import Any, Callable, Optional
 
 from omegaconf import DictConfig
 
-from ._internal.deprecation_warning import deprecation_warning
 from ._internal.utils import _run_hydra, get_args_parser
 from .types import TaskFunction
 
-_UNSPECIFIED_: Any = object()
-
 
 def main(
-    config_path: Optional[str] = _UNSPECIFIED_,
+    config_path: Optional[str] = None,
     config_name: Optional[str] = None,
 ) -> Callable[[TaskFunction], Any]:
     """
@@ -21,20 +17,6 @@ def main(
                         If config_path is None no directory is added to the Config search path.
     :param config_name: The name of the config (usually the file name without the .yaml extension)
     """
-
-    # DEPRECATED: remove in 1.2
-    # in 1.2, the default config_path should be changed to None
-    if config_path is _UNSPECIFIED_:
-        url = "https://hydra.cc/docs/next/upgrades/1.0_to_1.1/changes_to_hydra_main_config_path"
-        deprecation_warning(
-            message=dedent(
-                f"""
-            config_path is not specified in @hydra.main().
-            See {url} for more information."""
-            ),
-            stacklevel=2,
-        )
-        config_path = "."
 
     def main_decorator(task_function: TaskFunction) -> Callable[[], None]:
         @functools.wraps(task_function)
