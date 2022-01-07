49aa487bf24e58b149653d8788f03f29266829b8
Jasha10 <8935917+Jasha10@users.noreply.github.com>
2022-01-06 20:04:53 -0600

    Remove extraneous print statement

--- tests/test_apps/hydra_main_without_config_path/my_app.py
+++ tests/test_apps/hydra_main_without_config_path/my_app.py
@@ -8,7 +8,6 @@ from hydra.core.hydra_config import HydraConfig
 @hydra.main()
 def my_app(_: DictConfig) -> None:
     config_sources = HydraConfig.get().runtime.config_sources
-    print(config_sources)
     # Check that no directory has been added to the Config search path
     for source in config_sources:
         assert (
