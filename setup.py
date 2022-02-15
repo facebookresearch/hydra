510fde48eb28e1d5a211c9d38c4dfe517bf7f1c9
Adrish Dey <adrishd.cse2017@nsec.ac.in>
2022-02-16 03:57:07 +0530

    adding setup.py

---
+++ setup.py
@@ -0,0 +1,13 @@
+import setuptools
+
+
+setuptools.setup(
+    name="hydra-wandb-sweeper",
+    version="0.0.1",
+    author="Adrish Dey",
+    author_email="adrish@wandb.com",
+    description="Hydra sweeps with WandB sweeps",
+    packages=setuptools.find_namespace_packages(include=["hydra_plugins.*"]),
+    install_requires=["hydra-core", "wandb"],
+    include_package_data=True
+)
