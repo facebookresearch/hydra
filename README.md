063d1799064ca90224e79c2abd0af115830703ff
Mattie Tesfaldet <tesfaldet@hotmail.com>
2022-03-19 04:36:59 -0400

    A monolithic PR (#5)
    * Compatibility with hydra experiment overrides. Quantized step for distributions now supported. Sweep parameterization now more aligned with Hydra's standardized approach. Some bug fixes. Sweep overrides now support RangeSweep properly via quantized uniform. Wandb run now directly passed to task function instead of run id to avoid unnecessary init overhead.
    
    * Added some useful logs. Added some comments. Small changes.
    
    * Monkeypatching some wandb functions to allow for using wandb's code save when in a hydra cwd that's diff than the code's directory. Being more explicit with function arg types.
    
    * Can now pass in tags (as list) and notes via wandb_sweep_config yaml
    
    * Logging to .wandb instead of wandb, had to resort to a lot of monkeypatching but it was worth it. Some code cleanup and clarification.
    
    * Updated example. Fixed some comments. Making sure to use abs path for creating and using sweep directory. Updated README but still needs an update in the range section of categorical parameters since now it's using wandb's q_uniform.
    
    * Added agent budget functionality for launching batches of agents until budget reached. Added some comments. Retrieving results from task function via its agent so that it can be visible to the sweeper and launcher for inspecting JobReturn status and return value.
    
    * Returning results and statuses from task function to launcher. Improved task function error handling and propagation to launcher. Fixed bug with renaming wandb dir to .wandb when config_filepath has text 'wandb' anywhere that doesn't relate to a directory.
    
    * Moved main sweeper code to _impl.py so that it doesn't get accidentally loaded along with all its monkeypatching when the user doesn't want to use the sweeper. Upon running a task function, hydra loads all available plugins, even those that won't be used. Improved code typing. Added implicit start to yamlfmt pre-commit since it clashed with hydra's # @package _global_ indicator.
    
    * Improved error handling for agent failures such as sweep not existing anymore, job failures unrelated to the agent, and run failures within an agent.
    
    * Revamped example training script to showcase new pre-emption capabilities via the submitit launcher plugin. Note, not tested with Joblib, Ray, or RQ launcher plugins.
    
    * Support for Hyperband early termination. Graceful response towards remote killing/stopping of sweeps and runs.
    
    * More comments for clarity. Tweaking example so that it's easier to test out wandb alert during preemption. Slight code tweaks elswhere. Small pre-commit modifications.
    
    * Grid search method now supported for both config-style parameterization and override-style. Validation of metric arguments, early termination args, and search args.

--- README.md
+++ README.md
@@ -11,27 +11,13 @@ $ pip install git+https://github.com/captain-pool/hydra-wandb-sweeper.git
 
 Using this plugin is very simple, as one can just invoke the sweeper by overriding `hydra/sweeper` with `wandb`.
 
-The only difference this plugin has with respect to other regular hydra plugin, is the function signature of `main` requires the presence of an additional positional argument called `run_id`.
-
-Hence, the final function would something like:
 ```python
 @hydra.main(config_path=..., config_name=...)
-def main(cfg: DictConfig, run_id: str): # notice the extra run_id parameter
+def main(cfg: DictConfig):
 
   with wandb.init(id=run_id) as run:
     ...
     wandb.log({"loss": loss})
-```
-
-To make the main support other launchers as well, an user can mark the `run_id` parameter optional like the following:
-
-```python
-@hydra.main(config_path=..., config_name=...)
-def main(cfg: DictConfig, run_id: Optional[str] = None):
-
-with wandb.init(id=run_id) as run:
-  ...
-  wandb.log({"loss": loss})
 ```
 
 
--- hydra_plugins/hydra_wandb_sweeper/wandb_sweeper.py
+++ hydra_plugins/hydra_wandb_sweeper/wandb_sweeper.py
@@ -1,174 +1,32 @@
-import functools
-import itertools
-import logging
-import typing
+from importlib import import_module
+from typing import List, Optional
 
-import omegaconf
-import wandb
-from hydra.core import plugins
-from hydra.core.override_parser import overrides_parser, types
-from hydra.plugins import sweeper
+from hydra import TaskFunction
+from hydra.plugins.sweeper import Sweeper
+from hydra.types import HydraContext
+from omegaconf import DictConfig
 
-from hydra_plugins.hydra_wandb_sweeper import config
-
-SUPPORTED_DISTRIBUTIONS = {
-    "constant": ["value"],
-    "categorical": ["values"],
-    "int_uniform": ["min", "max"],
-    "uniform": ["min", "max"],
-    "q_uniform": ["min", "max"],
-    "log_uniform": ["min", "max"],
-    "q_log_uniform": ["min", "max"],
-    "inv_log_uniform": ["min", "max"],
-    "normal": ["mu", "sigma"],
-    "q_normal": ["mu", "sigma"],
-    "log_normal": ["mu", "sigma"],
-    "q_log_normal": ["mu", "sigma"],
-}
+from hydra_plugins.hydra_wandb_sweeper.config import WandbConfig
 
 
-def get_wandb_parameter(override):
+class WandbSweeper(Sweeper):
+    """Class to interface with Wandb"""
 
-    value = override.value()
+    def __init__(self, wandb_sweep_config: WandbConfig, params: Optional[DictConfig]):
+        _impl = import_module("hydra_plugins.hydra_wandb_sweeper._impl")
 
-    def set_parameter(parameter, distribution, *args):
-        keys = SUPPORTED_DISTRIBUTIONS[distribution]
-        parameter["distribution"] = distribution
-        for key, value in zip(keys, args):
-            parameter[key] = value
+        self.sweeper = _impl.WandbSweeperImpl(wandb_sweep_config, params)
 
-    distribution = None
-    if getattr(value, "tags", None):
-        assert len(value.tags) == 1
-        distribution = list(value.tags)[0]
-        if distribution not in SUPPORTED_DISTRIBUTIONS:
-            raise ValueError(
-                f"{distribution=} not supported. "
-                f"Supported Distributions: {list(SUPPORTED_DISTRIBUTIONS.keys())}"
-            )
-
-    parameter = {}
-
-    if not override.is_sweep_override():
-        distribution = "constant"
-        set_parameter(parameter, distribution, value)
-
-        return parameter
-
-    if override.is_interval_sweep():
-        distribution = distribution or "uniform"
-
-        if distribution in ("categorical", "constant"):
-            raise ValueError(
-                f"interval sweep doesn't support categorical/constant distribution"
-            )
-
-        set_parameter(parameter, distribution, value.start, value.end)
-        return parameter
-
-    if override.is_choice_sweep() or override.is_range_sweep():
-        choices = [
-            x for x in override.sweep_iterator(transformer=types.Transformer.encode)
-        ]
-
-        distribution = distribution or "categorical"
-
-        if distribution != "categorical":
-            raise ValueError(
-                f"Choice Sweep and Range Sweep doesn't allow {distribution=}"
-            )
-
-        set_parameter(parameter, distribution, choices)
-        return parameter
-
-    raise NotImplementedError(f"{override} not supported by WandB sweeper")
-
-
-class WandbSweeper(sweeper.Sweeper):
-    def __init__(self, wandb_sweep_config: omegaconf.DictConfig) -> None:
-        self.wandb_sweep_config = wandb_sweep_config
-        self._sweep_id = None
-        self._preemptible = wandb_sweep_config["preemptible"]
-        self.agent_run_count = self.wandb_sweep_config.get["count"]
-
-        self.job_idx = itertools.count(0)
-
-    def setup(self, config, hydra_context, task_function) -> None:
-        self.config = config
-
-        self._task_function = functools.partial(
-            self.wandb_task,
-            task_function=task_function,
-            count=self.agent_run_count,
+    def setup(
+        self,
+        *,
+        hydra_context: HydraContext,
+        task_function: TaskFunction,
+        config: DictConfig,
+    ) -> None:
+        return self.sweeper.setup(
+            hydra_context=hydra_context, task_function=task_function, config=config
         )
-        self.hydra_context = hydra_context
 
-        self.launcher = plugins.Plugins.instance().instantiate_launcher(
-            config=config,
-            hydra_context=hydra_context,
-            task_function=self._task_function,
-        )
-        self.sweep_dict = {
-            "name": self.wandb_sweep_config.name,
-            "method": self.wandb_sweep_config.method,
-            "parameters": {},
-        }
-        early_terminate = self.wandb_sweep_config.early_terminate
-        metric = self.wandb_sweep_config.metric
-
-        if metric:
-            self.sweep_dict.update({"metric": omegaconf.OmegaConf.to_container(metric)})
-
-        if early_terminate:
-            self.sweep_dict.update(
-                {"early_terminate": omegaconf.OmegaConf.to_container(early_terminate)}
-            )
-
-    @property
-    def sweep_id(self):
-        return self._sweep_id
-
-    def sweep(self, arguments):
-        parser = overrides_parser.OverridesParser.create(
-            config_loader=self.hydra_context.config_loader
-        )
-        parsed = parser.parse_overrides(arguments)
-        sweep_id = self.wandb_sweep_config["sweep_id"]
-
-        parameters = self.sweep_dict["parameters"]
-
-        for override in parsed:
-            parameters[override.get_key_element()] = get_wandb_parameter(override)
-
-        if not sweep_id:
-            sweep_id = wandb.sweep(
-                self.sweep_dict,
-                entity=self.wandb_sweep_config.entity,
-                project=self.wandb_sweep_config.project,
-            )
-        else:
-            logging.info(f"Reusing Sweep with ID: {sweep_id}")
-        self._sweep_id = sweep_id
-
-        overrides = []
-        for i in range(self.wandb_sweep_config.num_agents):
-            overrides.append(tuple(f"{key}=-1" for key in parameters))
-
-        self.launcher.launch(overrides, initial_job_idx=next(self.job_idx))
-
-    def wandb_task(self, base_config, task_function, count):
-        def run():
-            with wandb.init(resume=self._preemptible) as run:
-                if self._preemptible:
-                    wandb.mark_preempting()
-                run_id = run.id
-                override_config = run.config
-            override_config = omegaconf.DictConfig(override_config.as_dict())
-            config = omegaconf.OmegaConf.merge(base_config, override_config)
-            task_function(config, run_id)
-
-        if not self.sweep_id:
-            raise ValueError(f"sweep_id cannot be {self.sweep_id}")
-
-        logging.info("Launching agent")
-        wandb.agent(self.sweep_id, function=run, count=count)
+    def sweep(self, arguments: List[str]) -> None:
+        return self.sweeper.sweep(arguments)
