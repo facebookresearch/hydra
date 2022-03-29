80c12c0f3bd0c96947f53eb58751c1c15b8dce75
Jieru Hu <jieru@fb.com>
2022-03-29 15:45:00 -0700

    Add log job return callbacks

---
+++ hydra/experimental/callbacks.py
@@ -0,0 +1,25 @@
+# Copyright (c) Facebook, Inc. and its affiliates. All Rights Reserved
+
+import logging
+from typing import Any
+
+from omegaconf import DictConfig
+
+from hydra.core.utils import JobReturn, JobStatus
+from hydra.experimental.callback import Callback
+
+
+class LogJobReturnCallback(Callback):
+    def __init__(self) -> None:
+        self.log = logging.getLogger(f"{__name__}.{self.__class__.__name__}")
+
+    def on_job_end(
+        self, config: DictConfig, job_return: JobReturn, **kwargs: Any
+    ) -> None:
+        if job_return.status == JobStatus.COMPLETED:
+            self.log.info(f"Succeeded with return value: {job_return.return_value}")
+        elif job_return.status == JobStatus.FAILED:
+            self.log.error("", exc_info=job_return._return_value)
+        else:
+            self.log.error("Status unknown. This should never happen.")
+
