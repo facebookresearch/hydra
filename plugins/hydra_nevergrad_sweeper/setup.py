67f3346545e635f42d1bbf8a5744a0b1413d21e8
Jasha <8935917+Jasha10@users.noreply.github.com>
2022-09-08 20:08:51 -0500

    nevergrad-sweeper: support python3.10

--- plugins/hydra_nevergrad_sweeper/setup.py
+++ plugins/hydra_nevergrad_sweeper/setup.py
@@ -21,6 +21,7 @@ setup(
         "Programming Language :: Python :: 3.7",
         "Programming Language :: Python :: 3.8",
         "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
         "Operating System :: OS Independent",
         "Development Status :: 4 - Beta",
     ],
