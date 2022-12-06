c48ef19eb89f3171fba0c31ac9103c0f60c0ab79
Jasha10 <8935917+Jasha10@users.noreply.github.com>
2022-12-06 03:45:42 -0600

    python3.11 support for plugins (#2489)

--- examples/plugins/example_registered_plugin/setup.py
+++ examples/plugins/example_registered_plugin/setup.py
@@ -24,6 +24,7 @@ setup(
         "Programming Language :: Python :: 3.8",
         "Programming Language :: Python :: 3.9",
         "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.11",
         "Operating System :: OS Independent",
     ],
     install_requires=[
--- plugins/hydra_optuna_sweeper/setup.py
+++ plugins/hydra_optuna_sweeper/setup.py
@@ -22,6 +22,7 @@ setup(
         "Programming Language :: Python :: 3.8",
         "Programming Language :: Python :: 3.9",
         "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.11",
         "Operating System :: POSIX :: Linux",
         "Operating System :: MacOS",
         "Development Status :: 4 - Beta",
