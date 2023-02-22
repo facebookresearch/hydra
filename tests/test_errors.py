71e945d5ebc5a23e80b15ef7592bcc89f70397ea
Jasha Sommer-Simpson <8935917+Jasha10@users.noreply.github.com>
2023-02-21 22:29:00 -0600

    silence bandit security false positive (#2593)

--- tests/test_errors.py
+++ tests/test_errors.py
@@ -7,7 +7,7 @@ from hydra.errors import MissingConfigException
 def test_pickle_missing_config_exception() -> None:
     exception = MissingConfigException("msg", "filename", ["option1", "option2"])
     x = pickle.dumps(exception)
-    loaded = pickle.loads(x)
+    loaded = pickle.loads(x)  # nosec
     assert isinstance(loaded, MissingConfigException)
     assert loaded.args == ("msg",)
     assert loaded.missing_cfg_file == "filename"
