da36e03cd85caac75a3353f8f4b7e96996856bcd
Jessica Zhang (NY) <jess.jz.zhang@gmail.com>
2025-01-06 10:28:00 -0500

    Add test checking input config is unchanged

--- tests/instantiate/test_instantiate.py
+++ tests/instantiate/test_instantiate.py
@@ -419,8 +419,10 @@ def test_class_instantiate(
     recursive: bool,
 ) -> Any:
     passthrough["_recursive_"] = recursive
+    original_config_str = str(config)
     obj = instantiate_func(config, **passthrough)
     assert partial_equal(obj, expected)
+    assert str(config) == original_config_str
 
 
 def test_partial_with_missing(instantiate_func: Any) -> Any:
@@ -431,10 +433,12 @@ def test_partial_with_missing(instantiate_func: Any) -> Any:
         "b": 20,
         "c": 30,
     }
+    original_config_str = str(config)
     partial_obj = instantiate_func(config)
     assert partial_equal(partial_obj, partial(AClass, b=20, c=30))
     obj = partial_obj(a=10)
     assert partial_equal(obj, AClass(a=10, b=20, c=30))
+    assert str(config) == original_config_str
 
 
 def test_instantiate_with_missing(instantiate_func: Any) -> Any:
@@ -468,6 +472,7 @@ def test_none_cases(
             ListConfig(None),
         ],
     }
+    original_config_str = str(cfg)
     ret = instantiate_func(cfg)
     assert ret.kwargs["none_dict"] is None
     assert ret.kwargs["none_list"] is None
@@ -477,8 +482,10 @@ def test_none_cases(
     assert ret.kwargs["list"][0] == 10
     assert ret.kwargs["list"][1] is None
     assert ret.kwargs["list"][2] is None
+    assert str(cfg) == original_config_str
 
 
+@mark.parametrize("convert_to_list", [True, False])
 @mark.parametrize(
     "input_conf, passthrough, expected",
     [
@@ -537,6 +544,32 @@ def test_none_cases(
             6,
             id="interpolation_from_recursive",
         ),
+        param(
+            {
+                "my_id": 5,
+                "node": {
+                    "b": "${foo_b}",
+                },
+                "foo_b": {
+                    "unique_id": "${my_id}",
+                },
+            },
+            {},
+            OmegaConf.create({"b": {"unique_id": 5}}),
+            id="interpolation_from_parent_with_interpolation",
+        ),
+        param(
+            {
+                "my_id": 5,
+                "node": "${foo_b}",
+                "foo_b": {
+                    "unique_id": "${my_id}",
+                },
+            },
+            {},
+            OmegaConf.create({"unique_id": 5}),
+            id="interpolation_from_parent_with_interpolation",
+        ),
     ],
 )
 def test_interpolation_accessing_parent(
@@ -544,15 +577,24 @@ def test_interpolation_accessing_parent(
     input_conf: Any,
     passthrough: Dict[str, Any],
     expected: Any,
+    convert_to_list: bool,
 ) -> Any:
+    if convert_to_list:
+        input_conf = copy.deepcopy(input_conf)
+        input_conf["node"] = [input_conf["node"]]
     cfg_copy = OmegaConf.create(input_conf)
     input_conf = OmegaConf.create(input_conf)
-    obj = instantiate_func(input_conf.node, **passthrough)
+    original_config_str = str(input_conf)
+    if convert_to_list:
+        obj = instantiate_func(input_conf.node[0], **passthrough)
+    else:
+        obj = instantiate_func(input_conf.node, **passthrough)
     if isinstance(expected, partial):
         assert partial_equal(obj, expected)
     else:
         assert obj == expected
     assert input_conf == cfg_copy
+    assert str(input_conf) == original_config_str
 
 
 @mark.parametrize(
