9a6e787f01002583f3552fb1ee5e2dbf2a193017
Pei-Hsuan Wu <wu0607@meta.com>
2025-02-04 23:30:54 -0800

    Fix test

--- tests/test_hydra_cli_errors.py
+++ tests/test_hydra_cli_errors.py
@@ -19,41 +19,49 @@ chdir_hydra_root()
     [
         param(
             "+key=int(",
-            "no viable alternative at input 'int('",
+            """Error when parsing index: 1, string: +key=int( out of ['hydra.sweep.dir=/tmp/pytest-of-runner/pytest-0/test_cli_error_parse_error_in_0', '+key=int(', 'hydra.mode=RUN']. 
+no viable alternative at input 'int('""",
             id="parse_error_in_function",
         ),
         param(
             "+key=sort()",
-            """Error parsing override '+key=sort()'
+            """Error when parsing index: 1, string: +key=sort() out of ['hydra.sweep.dir=/tmp/pytest-of-runner/pytest-0/test_cli_error_empty_sort_0', '+key=sort()', 'hydra.mode=RUN']. 
+Error parsing override '+key=sort()'
 ValueError while evaluating 'sort()': empty sort input""",
             id="empty_sort",
         ),
         param(
             "key=sort(interval(1,10))",
-            """Error parsing override 'key=sort(interval(1,10))'
+            """Error when parsing index: 1, string: key=sort(interval(1,10)) out of ['hydra.sweep.dir=/tmp/pytest-of-runner/pytest-0/test_cli_error_sort_interval_0', 'key=sort(interval(1,10))', 'hydra.mode=RUN']. 
+Error parsing override 'key=sort(interval(1,10))'
 TypeError while evaluating 'sort(interval(1,10))': mismatch type argument args[0]""",
             id="sort_interval",
         ),
         param(
             "+key=choice()",
-            """Error parsing override '+key=choice()'
+            """Error when parsing index: 1, string: +key=choice() out of ['hydra.sweep.dir=/tmp/pytest-of-runner/pytest-0/test_cli_error_empty_choice0_0', '+key=choice()', 'hydra.mode=RUN']. 
+Error parsing override '+key=choice()'
 ValueError while evaluating 'choice()': empty choice is not legal""",
             id="empty choice",
         ),
         param(
             "+key=extend_list(1, 2, 3)",
-            """Error parsing override '+key=extend_list(1, 2, 3)'
+            """Error when parsing index: 1, string: +key=extend_list(1, 2, 3) out of ['hydra.sweep.dir=/tmp/pytest-of-runner/pytest-0/test_cli_error_plus_key_extend0', '+key=extend_list(1, 2, 3)', 'hydra.mode=RUN']. 
+Error parsing override '+key=extend_list(1, 2, 3)'
 Trying to use override symbols when extending a list""",
             id="plus key extend_list",
         ),
         param(
             "key={inner_key=extend_list(1, 2, 3)}",
-            "no viable alternative at input '{inner_key='",
+            """Error when parsing index: 1, string: key={inner_key=extend_list(1, 2, 3)} 
+out of ['hydra.sweep.dir=/tmp/pytest-of-runner/pytest-0/test_cli_error_embedded_extend0', 'key={inner_key=extend_list(1, 2, 3)}', 'hydra.mode=RUN']. 
+no viable alternative at input '{inner_key='""",
             id="embedded extend_list",
         ),
         param(
             ["+key=choice(choice(a,b))", "-m"],
-            """Error parsing override '+key=choice(choice(a,b))'
+            """Error when parsing index: 1, string: +key=choice(choice(a,b)) out of ['hydra.sweep.dir=/tmp/pytest-of-runner/pytest-0/test_cli_error_empty_choice1_0', '+key=choice(choice(a,b))', 'hydra.mode=MULTIRUN']. 
+Error parsing override '+key=choice(choice(a,b))'
 ValueError while evaluating 'choice(choice(a,b))': nesting choices is not supported
 See https://hydra.cc/docs/1.2/advanced/override_grammar/basic for details
 
