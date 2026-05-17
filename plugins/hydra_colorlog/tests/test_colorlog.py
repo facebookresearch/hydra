2e7974de73772e591faf9fc8ce7dc5505618d339
Omry Yadan <omry@yadan.net>
2026-05-17 15:27:12 +0800

    Fix colorlog example config loading

--- plugins/hydra_colorlog/tests/test_colorlog.py
+++ plugins/hydra_colorlog/tests/test_colorlog.py
@@ -1,8 +1,10 @@
-from hydra import initialize
-
 # Copyright (c) Facebook, Inc. and its affiliates. All Rights Reserved
+import os
+from pathlib import Path
+
+from hydra import initialize
 from hydra.core.global_hydra import GlobalHydra
-from hydra.test_utils.test_utils import chdir_plugin_root
+from hydra.test_utils.test_utils import chdir_plugin_root, run_python_script
 
 chdir_plugin_root()
 
@@ -18,3 +20,25 @@ def test_config_installed() -> None:
         config_loader = GlobalHydra.instance().config_loader()
         assert "colorlog" in config_loader.get_group_options("hydra/job_logging")
         assert "colorlog" in config_loader.get_group_options("hydra/hydra_logging")
+
+
+def test_example_app_loads_its_config(tmp_path: Path) -> None:
+    plugin_root = Path(__file__).resolve().parents[1]
+    env = os.environ.copy()
+    pythonpath = env.get("PYTHONPATH")
+    env["PYTHONPATH"] = (
+        str(plugin_root)
+        if pythonpath is None
+        else os.pathsep.join([str(plugin_root), pythonpath])
+    )
+    stdout, _stderr = run_python_script(
+        [
+            str(plugin_root / "example" / "my_app.py"),
+            f'hydra.run.dir="{tmp_path}"',
+            "hydra.job.chdir=false",
+        ],
+        env=env,
+    )
+
+    assert "Info level message" in stdout
+    assert (tmp_path / "my_app.log").exists()
